78a5d07ce00413e0d2349abb1d24edafc1bf93a6
Marek Hanzal <marek.hanzal@icloud.com>
2024-08-31 17:35:20 +0200

    Updated packages

--- app/auth/auth.server.ts
+++ app/auth/auth.server.ts
@@ -5,7 +5,7 @@ import { sessionStorage } from "~/session/session.server";
 export const authenticator = new Authenticator<Token>(sessionStorage);
 
 const getCallback = (name: string) => {
-	return `http://localhost:3000/auth/${provider}/callback`;
+	return `http://localhost:3000/auth/${name}/callback`;
 };
 
 // authenticator.use();
