adb5c9884762441d5b25126157e7bdde107ee85d
Marek Hanzal <marek.hanzal@icloud.com>
2025-09-09 20:08:56 +0200

    Refactor PageTransition component to streamline ghost state handling and improve animation consistency; update opacity settings and remove unnecessary console logs for cleaner code.

--- apps/zbav-se.me/src/app/ui/PageTransition.tsx
+++ apps/zbav-se.me/src/app/ui/PageTransition.tsx
@@ -1,11 +1,11 @@
 import { useRouterState } from "@tanstack/react-router";
 import { AnimatePresence, motion } from "motion/react";
 import {
-	type FC,
-	type PropsWithChildren,
-	useLayoutEffect,
-	useRef,
-	useState,
+    type FC,
+    type PropsWithChildren,
+    useLayoutEffect,
+    useRef,
+    useState,
 } from "react";
 
 export const PageTransition: FC<PropsWithChildren> = ({ children }) => {
@@ -23,48 +23,38 @@ export const PageTransition: FC<PropsWithChildren> = ({ children }) => {
 	useLayoutEffect(() => {
 		if (status === "pending") {
 			ghostRef.current = outletRef.current?.innerHTML;
-		} else if (status === "idle") {            
 			setTransition("ghost");
 		}
 	}, [
 		status,
 	]);
 
-	console.log("transition", {
-		status,
-		transition,
-		ghostRef: ghostRef.current,
-	});
-
 	return (
 		<AnimatePresence mode="wait">
 			{transition === "ghost" ? (
-				<>
-					Ghost time
-					<motion.div
-						key={`ghost:${pathname}`}
-						initial={{
-							opacity: 0,
-						}}
-						animate={{
-							opacity: 1,
-						}}
-						exit={{
-							opacity: 0,
-						}}
-						transition={{
-							duration: 1.5,
-							ease: "easeInOut",
-						}}
-						onAnimationComplete={() => {
-							console.log("animation complete");
-							setTransition("outlet");
-						}}
-						dangerouslySetInnerHTML={{
-							__html: ghostRef.current ?? "",
-						}}
-					/>
-				</>
+				<motion.div
+					key={`ghost:${pathname}`}
+					initial={{
+						opacity: 1,
+					}}
+					animate={{
+						opacity: 1,
+					}}
+					exit={{
+						opacity: 0,
+					}}
+					transition={{
+						duration: 1.5,
+						ease: "easeInOut",
+					}}
+					onAnimationComplete={() => {
+						console.log("animation complete");
+						setTransition("outlet");
+					}}
+					dangerouslySetInnerHTML={{
+						__html: ghostRef.current ?? "nobody here",
+					}}
+				/>
 			) : null}
 
 			{transition === "outlet" ? (
@@ -85,12 +75,6 @@ export const PageTransition: FC<PropsWithChildren> = ({ children }) => {
 						rotateY: 0,
 						rotateX: 0,
 						rotateZ: 0,
-					}}
-					exit={{
-						opacity: 0,
-						// x: "-25%",
-						// y: "-25%",
-						rotateZ: 30,
 					}}
 					transition={{
 						duration: 1.5,
