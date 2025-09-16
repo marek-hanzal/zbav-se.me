1309f9b3dfe164850e8189f7fb729d2cae2f4c08
Marek Hanzal <marek.hanzal@icloud.com>
2025-09-16 20:37:15 +0200

    Enhance Container component: introduce overflow options and adjust grid settings for horizontal and vertical orientations. Update Route component layout to include new structure for improved item display.

--- apps/zbav-se.me/src/@routes/$locale/n/test.tsx
+++ apps/zbav-se.me/src/@routes/$locale/n/test.tsx
@@ -4,10 +4,22 @@ import { Container } from "~/app/ui/container/Container";
 export const Route = createFileRoute("/$locale/n/test")({
 	component() {
 		return (
-			<Container orientation="horizontal-full">
-				<Container item="full">Jebka 1</Container>
-				<Container item="full">Jebka 2</Container>
-				<Container item="full">Jebka 3</Container>
+			<Container height={"full"}>
+				<div className="h-1/2">
+					<Container orientation="horizontal-full">
+						<Container item="full">Jebka 1</Container>
+						<Container item="full">Jebka 2</Container>
+						<Container item="full">Jebka 3</Container>
+					</Container>
+				</div>
+
+				<div className="h-1/2">
+					<Container orientation="vertical-full">
+						<Container item="full">Jebka 1</Container>
+						<Container item="full">Jebka 2</Container>
+						<Container item="full">Jebka 3</Container>
+					</Container>
+				</div>
 			</Container>
 		);
 	},
