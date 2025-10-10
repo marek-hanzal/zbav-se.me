54609ebe9f86f6617757dd558618948bae1e90bb
Marek Hanzal <marek.hanzal@icloud.com>
2025-10-10 17:43:45 +0200

    Add additional path to Tailwind CSS configuration for improved content scanning

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -4,6 +4,7 @@ export default {
 	content: [
 		"../../packages/**/*.{ts,tsx}",
 		"./node_modules/@use-pico/client/**/*.{ts,tsx}",
+		"../../node_modules/@use-pico/client/**/*.{ts,tsx}",
 		"./src/**/*.{ts,tsx}",
 	],
 	plugins: [
