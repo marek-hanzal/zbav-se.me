e8752aeaf587095a833b57156ac57df1e82aa5a7
Marek Hanzal <marek.hanzal@icloud.com>
2025-10-11 16:12:05 +0200

    Enhance server application with additional middleware and body size limit
    - Integrated request ID and secure headers middleware for improved request tracking and security.
    - Added body size limit middleware to restrict incoming request sizes, optimizing resource usage.
    - Reorganized route definitions for clarity and maintainability.

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,5 +1,8 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { bodyLimit } from "hono/body-limit";
 import { cors } from "hono/cors";
+import { requestId } from "hono/request-id";
+import { secureHeaders } from "hono/secure-headers";
 import { categoryRoot } from "./category/categoryRoot";
 import { categoryGroupRoot } from "./category-group/categoryGroupRoot";
 import { healthRoot } from "./health/healthRoute";
@@ -9,15 +12,9 @@ import { withOpenApi } from "./open-api/withOpenApi";
 const app = withOpenApi(new OpenAPIHono());
 
 //
-app.route("/", categoryGroupRoot);
-app.route("/", categoryRoot);
-app.route("/", migrationRoot);
-app.route("/", healthRoot);
-//
-
-//
+app.use(requestId());
+app.use(secureHeaders());
 app.use(
-	"*",
 	cors({
 		origin: "*",
 		allowHeaders: [
@@ -29,16 +26,30 @@ app.use(
 			"POST",
 			"PUT",
 			"DELETE",
+			"PATCH",
 			"OPTIONS",
 		],
 		exposeHeaders: [
-			"Content-Type",
-			"Authorization",
+			"Content-Length",
+			"X-Request-Id",
 		],
 		maxAge: 600,
-		credentials: true,
+		credentials: false,
 	}),
 );
+app.use(
+	bodyLimit({
+		// We don't need to accept large large body as file uploads will be done directly to the Storage
+		maxSize: 1024 * 50,
+	}),
+);
+//
+
+//
+app.route("/", categoryGroupRoot);
+app.route("/", categoryRoot);
+app.route("/", migrationRoot);
+app.route("/", healthRoot);
 //
 
 export default app;
