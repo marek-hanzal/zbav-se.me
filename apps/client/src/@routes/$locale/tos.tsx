14fe72e76f0bd4a4cddd6c2290ae84d762f72714
Marek Hanzal <marek.hanzal@icloud.com>
2025-10-12 17:25:08 +0200

    Add React Markdown support for Terms of Service
    - Integrated `react-markdown` and `remark-gfm` for rendering markdown content in the Terms of Service route.
    - Updated the Terms of Service component to load markdown files dynamically based on the user's locale.
    - Created new markdown files for Czech and English versions of the Terms of Service.
    - Enhanced the UI to display markdown content with appropriate styling and structure.

--- apps/client/src/@routes/$locale/tos.tsx
+++ apps/client/src/@routes/$locale/tos.tsx
@@ -1,14 +1,59 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Container } from "@use-pico/client";
+import { createFileRoute, notFound } from "@tanstack/react-router";
+import { Container, Typo } from "@use-pico/client";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+const tos = import.meta.glob("/src/tos/*.md", {
+	as: "raw",
+});
 
 export const Route = createFileRoute("/$locale/tos")({
+	async loader({ params: { locale } }) {
+		const list = [
+			`/src/tos/${locale}.md`,
+			"/src/tos/cs.md",
+		];
+
+		for (const key of list) {
+			const loader = tos[key];
+			if (loader) {
+				return await loader();
+			}
+		}
+
+		throw notFound();
+	},
 	component() {
+		const markdown = Route.useLoaderData();
+
 		return (
 			<Container
 				layout={"vertical"}
 				overflow={"vertical"}
+				tone={"primary"}
+				theme={"light"}
+				round={"xl"}
+				square={"lg"}
 			>
-				The boring stuff here
+				<ReactMarkdown
+					remarkPlugins={[
+						remarkGfm,
+					]}
+					components={{
+						h1({ children }) {
+							return (
+								<Typo
+									label={children}
+									size={"xl"}
+									font={"bold"}
+									tone={"primary"}
+								/>
+							);
+						},
+					}}
+				>
+					{markdown}
+				</ReactMarkdown>
 			</Container>
 		);
 	},
---
+++ apps/client/src/tos/cs.md
@@ -0,0 +1,1 @@
+# Podminky, pyco!
