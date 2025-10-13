6f78782af3301a7468dd15217bdf956382bdc3ef
Marek Hanzal <marek.hanzal@icloud.com>
2025-10-13 16:05:56 +0200

    Refactor locale route to streamline translation loading
    - Simplified translation loading by removing the isomorphic function and directly importing translation files based on the locale.
    - Enhanced the route component to utilize the Outlet for better structure and maintainability.
    - Improved error handling by consolidating translation logic within the loader function.

--- apps/client/src/@routes/$locale.tsx
+++ apps/client/src/@routes/$locale.tsx
@@ -1,26 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { createIsomorphicFn } from "@tanstack/react-start";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { translator } from "@use-pico/common";
-
-const translationFn = createIsomorphicFn()
-	.server(async (locale: string) => {
-		translator.push(
-			(await import(`../translation/${locale}.yaml`)).default,
-		);
-	})
-	.client(async (locale: string) => {
-		translator.push(
-			(await import(`../translation/${locale}.yaml`)).default,
-		);
-	});
 
 export const Route = createFileRoute("/$locale")({
 	async loader({ params: { locale } }) {
-		try {
-			console.log("loader", locale);
-			await translationFn(locale);
-		} catch (e) {
-			console.error(e);
-		}
+		const dict = (await import(`../translation/${locale}.yaml`)).default;
+
+		translator.push(dict);
+
+		return dict;
+	},
+	component() {
+		const translations = Route.useLoaderData();
+		translator.push(translations);
+
+		return <Outlet />;
 	},
 });
