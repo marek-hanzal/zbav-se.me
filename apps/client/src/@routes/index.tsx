259e5d4838b36e66634bbaab469083d128619289
Marek Hanzal <marek.hanzal@icloud.com>
2025-10-13 21:22:53 +0200

    Update dependencies and enhance locale handling in route
    - Added `@escapace/accept-language-parser` to manage locale selection based on the `accept-language` header.
    - Updated `@tanstack/react-query` and `@use-pico/client` to their latest versions for improved stability.
    - Refactored the route loader to utilize a server function for locale determination, enhancing the routing mechanism.

--- apps/client/src/@routes/index.tsx
+++ apps/client/src/@routes/index.tsx
@@ -1,16 +1,24 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import { localeOf } from "@use-pico/common";
+import { createServerFn } from "@tanstack/react-start";
+import { getRequestHeader } from "@tanstack/react-start/server";
 import { defaultLocale, locales } from "~/locales";
 
+const serverFn = createServerFn().handler(async () => {
+	const { pick } = await import("@escapace/accept-language-parser");
+
+	const [locale] = pick(getRequestHeader("accept-language") ?? "", locales, {
+		type: "lookup",
+	});
+
+	return locale ?? defaultLocale;
+});
+
 export const Route = createFileRoute("/")({
-	loader() {
+	async loader() {
 		throw redirect({
 			to: "/$locale",
 			params: {
-				locale: localeOf({
-					available: locales,
-					fallback: defaultLocale,
-				}),
+				locale: await serverFn(),
 			},
 		});
 	},
