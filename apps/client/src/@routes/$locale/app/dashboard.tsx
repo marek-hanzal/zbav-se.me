a618e731a75bcbbc728132ad43661f8497edc942
Marek Hanzal <marek.hanzal@icloud.com>
2025-10-14 21:25:19 +0200

    Enhance dashboard component with navigation links for improved user experience
    - Added navigation links to create a listing and access user settings within the dashboard.
    - Utilized the LinkTo component for dynamic routing based on the current locale.

--- apps/client/src/@routes/$locale/app/dashboard.tsx
+++ apps/client/src/@routes/$locale/app/dashboard.tsx
@@ -1,8 +1,30 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { LinkTo } from "@use-pico/client";
 import { Sheet } from "~/app/sheet/Sheet";
 
 export const Route = createFileRoute("/$locale/app/dashboard")({
 	component() {
-		return <Sheet>Tiles will be here, bro!</Sheet>;
+		const { locale } = Route.useParams();
+
+		return (
+			<Sheet>
+				<LinkTo
+					to="/$locale/app/listing/create"
+					params={{
+						locale,
+					}}
+				>
+					[Create listing]
+				</LinkTo>
+				<LinkTo
+					to="/$locale/app/user"
+					params={{
+						locale,
+					}}
+				>
+					[User]
+				</LinkTo>
+			</Sheet>
+		);
 	},
 });
