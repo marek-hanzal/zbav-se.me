c4cbb41b34ebcd49f5d4b876699489ad97435fe7
Marek Hanzal <marek.hanzal@icloud.com>
2025-10-15 01:29:17 +0200

    Add PrimaryOverlay component to listing creation route for enhanced UI

--- apps/client/src/@routes/$locale/app/listing/create.tsx
+++ apps/client/src/@routes/$locale/app/listing/create.tsx
@@ -2,11 +2,14 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Container } from "@use-pico/client";
 import { CreateListingProvider } from "~/app/listing/context/CreateListingProvider";
 import { CreateListing } from "~/app/listing/ui/CreateListing";
+import { PrimaryOverlay } from "~/app/ui/overlay/PrimaryOverlay";
 
 export const Route = createFileRoute("/$locale/app/listing/create")({
 	component() {
 		return (
 			<Container layout={"vertical-content-footer"}>
+				<PrimaryOverlay opacity={"50"} />
+
 				<CreateListingProvider photoCountLimit={10}>
 					<CreateListing />
 				</CreateListingProvider>
