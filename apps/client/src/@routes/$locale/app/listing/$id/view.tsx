55fa77043d12c4450a2256a5e78dfb3fad0d4b88
Marek Hanzal <marek.hanzal@icloud.com>
2025-10-19 21:05:51 +0200

    Enhance listing view component with container and overlay
    - Wrapped the listing view component in a Container for improved layout and styling.
    - Added PrimaryOverlay to enhance the user interface experience.
    - Maintained existing functionality for displaying listing data while improving overall structure.

--- apps/client/src/@routes/$locale/app/listing/$id/view.tsx
+++ apps/client/src/@routes/$locale/app/listing/$id/view.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Data } from "@use-pico/client";
+import { Container, Data } from "@use-pico/client";
 import { withListingFetchQuery } from "~/app/listing/query/withListingFetchQuery";
 import { Sheet } from "~/app/sheet/Sheet";
+import { PrimaryOverlay } from "~/app/ui/overlay/PrimaryOverlay";
 
 export const Route = createFileRoute("/$locale/app/listing/$id/view")({
 	component() {
@@ -13,14 +14,22 @@ export const Route = createFileRoute("/$locale/app/listing/$id/view")({
 		});
 
 		return (
-			<Sheet>
-				<Data
-					result={listingQuery}
-					renderSuccess={({ data }) => {
-						return data.id;
-					}}
-				/>
-			</Sheet>
+			<Container
+				square={"md"}
+				tone={"secondary"}
+				theme={"light"}
+			>
+				<PrimaryOverlay />
+
+				<Sheet>
+					<Data
+						result={listingQuery}
+						renderSuccess={({ data }) => {
+							return data.id;
+						}}
+					/>
+				</Sheet>
+			</Container>
 		);
 	},
 });
