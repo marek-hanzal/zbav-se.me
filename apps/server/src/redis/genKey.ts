a169ac14b175a39d0a8046f5f218dd7cba2f925a
Marek Hanzal <marek.hanzal@icloud.com>
2025-10-28 23:02:39 +0100

    Fix key generation format in genKey function to ensure proper delimiter usage between domain, scope, and version.

--- apps/server/src/redis/genKey.ts
+++ apps/server/src/redis/genKey.ts
@@ -9,5 +9,5 @@ export namespace genKey {
 }
 
 export const genKey = ({ scope, version, value }: genKey.Props) => {
-	return `${AppEnv.VITE_DOMAIN}${scope}:${version}:${JSON.stringify(value)}`;
+	return `${AppEnv.VITE_DOMAIN}:${scope}:${version}:${JSON.stringify(value)}`;
 };
