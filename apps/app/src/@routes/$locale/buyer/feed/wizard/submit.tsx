60da8b7c248cf4d5eec11e7b9541bc6671f78b8f
Marek Hanzal <marek.hanzal@icloud.com>
2025-10-31 17:22:50 +0100

    Enhance buyer feed wizard submit component with improved UI and functionality
    - Refactored the submit component to include additional UI elements such as badges and spinners for better user feedback.
    - Integrated location and category queries to dynamically display selected options.
    - Updated navigation handling and submission logic to streamline user interactions.
    - Set SSR to false for the submit route to optimize performance.

--- apps/app/src/@routes/$locale/buyer/feed/wizard/submit.tsx
+++ apps/app/src/@routes/$locale/buyer/feed/wizard/submit.tsx
@@ -1,17 +1,78 @@
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { ArrowLeftIcon, Button, LinkTo, Status } from "@use-pico/client";
+import { createFileRoute } from "@tanstack/react-router";
+import {
+	ArrowLeftIcon,
+	Badge,
+	type BadgeCls,
+	Button,
+	Container,
+	LinkTo,
+	SpinnerIcon,
+	Status,
+	Tx,
+	Typo,
+} from "@use-pico/client";
+import { type Cls, VariantProvider } from "@use-pico/cls";
 import type { tFeedCreate, tFeedPatch } from "@zbav-se.me/sdk";
-import { FeedIcon, TitleContainer } from "@zbav-se.me/ui";
+import { SendPackageIcon, ThemeCls, TitleContainer } from "@zbav-se.me/ui";
+import { withCategoryCollectionQuery } from "~/app/category/query/withCategoryCollectionQuery";
 import { withFeedCreateMutation } from "~/app/feed/mutation/withFeedCreateMutation";
 import { withFeedPatchMutation } from "~/app/feed/mutation/withFeedPatchMutation";
 import { FeedWizardSchema } from "~/app/feed/schema/FeedWizardSchema";
+import { withLocationFetchQuery } from "~/app/location/query/withLocationFetchQuery";
 
 export const Route = createFileRoute("/$locale/buyer/feed/wizard/submit")({
 	validateSearch: FeedWizardSchema,
+	ssr: false,
+	pendingComponent() {
+		const { locale } = Route.useParams();
+		const state = Route.useSearch();
+
+		return (
+			<TitleContainer
+				textTitle={"Feed submit (title)"}
+				left={
+					<LinkTo
+						icon={ArrowLeftIcon}
+						to={"/$locale/buyer/feed/wizard/name"}
+						search={state}
+						params={{
+							locale,
+						}}
+						tone={"secondary"}
+					/>
+				}
+			>
+				<Status icon={SpinnerIcon} />
+			</TitleContainer>
+		);
+	},
 	component() {
 		const { locale } = Route.useParams();
 		const state = Route.useSearch();
-		const navigate = useNavigate();
+		const navigate = Route.useNavigate();
+
+		const locationFetchQuery = withLocationFetchQuery().useSuspenseQuery(
+			{
+				where: {
+					id: state.locationId,
+				},
+			},
+			{
+				enabled: !!state.locationId,
+			},
+		);
+
+		const categoryCollectionQuery =
+			withCategoryCollectionQuery.useSuspenseQuery(
+				{
+					where: {
+						idIn: state.filter?.categoryIdIn,
+					},
+				},
+				{
+					enabled: !!state.filter?.categoryIdIn?.length,
+				},
+			);
 
 		const feedCreateMutation = withFeedCreateMutation.useMutation({
 			async onPostMutation({ result }) {
@@ -44,6 +105,24 @@ export const Route = createFileRoute("/$locale/buyer/feed/wizard/submit")({
 		const isLoading =
 			feedCreateMutation.isPending || feedPatchMutation.isPending;
 
+		const badgeTweak: Cls.TweaksOf<BadgeCls> = {
+			slot: {
+				root: {
+					class: [
+						"flex",
+						"flex-col",
+						"items-start",
+						"h-fit",
+						"w-full",
+					],
+					token: [
+						"round.md",
+						"square.md",
+					],
+				},
+			},
+		};
+
 		return (
 			<TitleContainer
 				textTitle={"Feed submit (title)"}
@@ -51,43 +130,323 @@ export const Route = createFileRoute("/$locale/buyer/feed/wizard/submit")({
 					<LinkTo
 						icon={ArrowLeftIcon}
 						to={"/$locale/buyer/feed/wizard/name"}
+						search={state}
 						params={{
 							locale,
-						}}
-						search={{
-							...state,
 						}}
 						tone={"secondary"}
 					/>
 				}
-			>
-				<Status
-					icon={FeedIcon}
-					textTitle={"Feed submit - all set (title)"}
-					action={
-						<Button
-							label={"Submit - feed (button)"}
-							tone={"secondary"}
-							theme={"dark"}
-							size={"lg"}
-							disabled={isLoading}
-							loading={isLoading}
-							onClick={() => {
-								/**
-								 * Validation is done in the mutation itself.
-								 */
-								if (state.id) {
-									feedPatchMutation.mutate(
-										state as tFeedPatch,
-									);
-									return;
-								}
+				bottom={
+					<Button
+						iconEnabled={SendPackageIcon}
+						label={"Submit - feed (button)"}
+						disabled={isLoading}
+						loading={isLoading}
+						tone={"primary"}
+						theme={"dark"}
+						size={"lg"}
+						full
+						onClick={() => {
+							if (state.id) {
+								feedPatchMutation.mutate(state as tFeedPatch);
+								return;
+							}
 
-								feedCreateMutation.mutate(state as tFeedCreate);
+							feedCreateMutation.mutate(state as tFeedCreate);
+						}}
+					/>
+				}
+			>
+				<Container
+					layout={"vertical-flex"}
+					scroll={"vertical"}
+					gap={"md"}
+					height={"fit"}
+					width={"fit"}
+				>
+					<VariantProvider
+						cls={ThemeCls}
+						variant={{
+							tone: "primary",
+							theme: "light",
+						}}
+					>
+						<LinkTo
+							to={"/$locale/buyer/feed/wizard/name"}
+							params={{
+								locale,
 							}}
-						/>
-					}
-				/>
+							search={state}
+							display={"block"}
+							full
+						>
+							<Badge tweak={badgeTweak}>
+								<Tx
+									label={"Feed name (label)"}
+									preset={"label"}
+								/>
+
+								<Typo label={state.name ?? "Not selected"} />
+							</Badge>
+						</LinkTo>
+
+						<LinkTo
+							to={"/$locale/buyer/feed/wizard/location"}
+							params={{
+								locale,
+							}}
+							search={state}
+							display={"block"}
+							full
+							disabled={!locationFetchQuery.data}
+						>
+							<Badge
+								disabled={!locationFetchQuery.data}
+								tweak={badgeTweak}
+							>
+								<Tx
+									label={"Feed location (label)"}
+									preset={"label"}
+								/>
+
+								{locationFetchQuery.data ? (
+									<Typo
+										label={locationFetchQuery.data.address}
+										wrap={"wrap"}
+									/>
+								) : (
+									<Typo label={"Not selected"} />
+								)}
+							</Badge>
+						</LinkTo>
+
+						<LinkTo
+							to={"/$locale/buyer/feed/wizard/sort"}
+							params={{
+								locale,
+							}}
+							search={state}
+							display={"block"}
+							full
+							disabled={!state.sort || state.sort.length === 0}
+						>
+							{state.sort && state.sort.length > 0 ? (
+								<>
+									<Badge
+										theme={"light"}
+										tweak={badgeTweak}
+									>
+										<Tx
+											label={"Feed sorting (label)"}
+											preset={"label"}
+										/>
+									</Badge>
+									<Container
+										layout={"vertical-flex"}
+										gap={"sm"}
+										square={"sm"}
+									>
+										{state.sort.map((sortItem, index) => (
+											<LinkTo
+												key={`${sortItem.value}-${index}`}
+												to={
+													"/$locale/buyer/feed/wizard/sort"
+												}
+												params={{
+													locale,
+												}}
+												search={state}
+												display={"block"}
+												full
+											>
+												<Badge
+													tone={"secondary"}
+													theme={"light"}
+													tweak={badgeTweak}
+												>
+													<Tx
+														label={`Listing common sort value ${sortItem.value} - ${sortItem.sort}`}
+													/>
+												</Badge>
+											</LinkTo>
+										))}
+									</Container>
+								</>
+							) : (
+								<Badge
+									disabled
+									tweak={badgeTweak}
+								>
+									<Tx
+										label={"Feed sorting (label)"}
+										preset={"label"}
+									/>
+									<Typo label={"Not selected"} />
+								</Badge>
+							)}
+						</LinkTo>
+
+						<LinkTo
+							to={"/$locale/buyer/feed/wizard/category"}
+							params={{
+								locale,
+							}}
+							search={state}
+							display={"block"}
+							full
+							disabled={
+								(categoryCollectionQuery.data?.data ?? [])
+									.length === 0
+							}
+						>
+							{(categoryCollectionQuery.data?.data ?? []).length >
+							0 ? (
+								<>
+									<Badge
+										theme={"light"}
+										tweak={badgeTweak}
+									>
+										<Tx
+											label={"Feed category (label)"}
+											preset={"label"}
+										/>
+									</Badge>
+									<Container
+										layout={"vertical-flex"}
+										gap={"sm"}
+										square={"sm"}
+									>
+										{(
+											categoryCollectionQuery.data
+												?.data ?? []
+										).map((category) => (
+											<LinkTo
+												key={category.id}
+												to={
+													"/$locale/buyer/feed/wizard/category"
+												}
+												params={{
+													locale,
+												}}
+												search={state}
+												display={"block"}
+												full
+											>
+												<Badge
+													tone={"secondary"}
+													theme={"light"}
+													tweak={badgeTweak}
+												>
+													<div
+														className={
+															"flex flex-col gap-0.5 items-start"
+														}
+													>
+														<Typo
+															label={
+																category.group
+															}
+															size={"xs"}
+														/>
+														<Typo
+															label={
+																category.category
+															}
+														/>
+													</div>
+												</Badge>
+											</LinkTo>
+										))}
+									</Container>
+								</>
+							) : (
+								<Badge
+									disabled
+									tweak={badgeTweak}
+								>
+									<Tx
+										label={"Feed category (label)"}
+										preset={"label"}
+									/>
+									<Typo label={"Not selected"} />
+								</Badge>
+							)}
+						</LinkTo>
+
+						<LinkTo
+							to={"/$locale/buyer/feed/wizard/condition"}
+							params={{
+								locale,
+							}}
+							search={state}
+							display={"block"}
+							full
+							disabled={
+								!state.filter?.conditionIn ||
+								state.filter.conditionIn.length === 0
+							}
+						>
+							{state.filter?.conditionIn &&
+							state.filter.conditionIn.length > 0 ? (
+								<>
+									<Badge
+										theme={"light"}
+										tweak={badgeTweak}
+									>
+										<Tx
+											label={"Feed condition (label)"}
+											preset={"label"}
+										/>
+									</Badge>
+									<Container
+										layout={"vertical-flex"}
+										gap={"sm"}
+										square={"sm"}
+									>
+										{state.filter.conditionIn.map(
+											(condition) => (
+												<LinkTo
+													key={condition}
+													to={
+														"/$locale/buyer/feed/wizard/condition"
+													}
+													params={{
+														locale,
+													}}
+													search={state}
+													display={"block"}
+													full
+												>
+													<Badge
+														tone={"secondary"}
+														theme={"light"}
+														tweak={badgeTweak}
+													>
+														<Tx
+															label={`Condition - Overall [${condition}] (hint)`}
+														/>
+													</Badge>
+												</LinkTo>
+											),
+										)}
+									</Container>
+								</>
+							) : (
+								<Badge
+									disabled
+									tweak={badgeTweak}
+								>
+									<Tx
+										label={"Feed condition (label)"}
+										preset={"label"}
+									/>
+									<Typo label={"Not selected"} />
+								</Badge>
+							)}
+						</LinkTo>
+					</VariantProvider>
+				</Container>
 			</TitleContainer>
 		);
 	},
