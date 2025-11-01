aa0bfee51cba4dec0aaec2bb5c203aa4ff02e60d
Marek Hanzal <marek.hanzal@icloud.com>
2025-11-01 14:03:13 +0100

    Update bundle size records for app, server, and web components with additional entries reflecting builds from November 1, 2025.

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -51,10 +51,10 @@ export default defineConfig(({ mode }) => {
 		build: {
 			target: "esnext",
 			assetsDir: "assets",
-			// assetsInlineLimit: 0,
-			// minify: "esbuild",
-			// sourcemap: false,
-			// manifest: false,
+			assetsInlineLimit: 0,
+			minify: "esbuild",
+			sourcemap: false,
+			manifest: false,
 			// rollupOptions: {
 			// 	treeshake: true,
 			// 	output: {
@@ -79,8 +79,8 @@ export default defineConfig(({ mode }) => {
 			// 	},
 			// },
 		},
-		// json: {
-		// 	stringify: true,
-		// },
+		json: {
+			stringify: true,
+		},
 	};
 });
