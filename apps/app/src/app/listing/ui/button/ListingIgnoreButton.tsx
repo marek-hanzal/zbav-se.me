832accdb0b90013f091b48b52ccfc509cc1bb7eb
Marek Hanzal <marek.hanzal@icloud.com>
2025-11-08 03:06:37 +0100

    Implement toast notifications for listing ignore actions and update translations
    - Added toast notifications to the `ListingIgnoreButton` component to provide user feedback on ignore and unignore actions.
    - Introduced new translation entries for toast messages in both English and Czech, enhancing localization support for user interactions.
    - Updated button click handling to display contextual messages based on the current ignore state of the listing.

--- apps/app/src/app/listing/ui/button/ListingIgnoreButton.tsx
+++ apps/app/src/app/listing/ui/button/ListingIgnoreButton.tsx
@@ -1,7 +1,9 @@
 import { ConfirmButton } from "@use-pico/client/ui/button";
+import { translator } from "@use-pico/common/translator";
 import { withListingIgnoreToggleMutation } from "@zbav-se.me/sdk/mutation";
 import { CancelIcon } from "@zbav-se.me/ui/icon";
 import type { FC } from "react";
+import { toast } from "sonner";
 
 export namespace ListingIgnoreButton {
 	export interface Props extends ConfirmButton.Props {
@@ -16,12 +18,21 @@ export const ListingIgnoreButton: FC<ListingIgnoreButton.Props> = ({
 	isIgnored,
 	onSuccess,
 	confirmProps,
+	buttonProps,
+	onReset,
 	...props
 }) => {
 	const listingIgnoreToggleMutation =
 		withListingIgnoreToggleMutation.useMutation({
 			onSuccess() {
 				onSuccess(!isIgnored);
+				toast.success(
+					translator.text(
+						isIgnored
+							? "Listing unignored (toast)"
+							: "Listing ignored (toast)",
+					),
+				);
 			},
 			meta: {
 				mutationId: listingId,
@@ -34,6 +45,34 @@ export const ListingIgnoreButton: FC<ListingIgnoreButton.Props> = ({
 			tone={"primary"}
 			theme={isIgnored ? "dark" : "light"}
 			loading={listingIgnoreToggleMutation.isPending}
+			buttonProps={{
+				...buttonProps,
+				onClick(event) {
+					if (isIgnored) {
+						toast.info(
+							translator.text(
+								"Second tap to unignore listing (toast)",
+							),
+							{
+								id: "listing-ignore-button",
+							},
+						);
+					}
+
+					if (!isIgnored) {
+						toast.info(
+							translator.text(
+								"Second tap to ignore listing (toast)",
+							),
+							{
+								id: "listing-ignore-button",
+							},
+						);
+					}
+
+					buttonProps?.onClick?.(event);
+				},
+			}}
 			confirmProps={{
 				tone: confirmProps?.tone ?? "secondary",
 				theme: confirmProps?.theme ?? "dark",
@@ -44,6 +83,10 @@ export const ListingIgnoreButton: FC<ListingIgnoreButton.Props> = ({
 						listingId,
 					});
 				},
+			}}
+			onReset={() => {
+				onReset?.();
+				toast.dismiss("listing-ignore-button");
 			}}
 			round={"full"}
 			{...props}
