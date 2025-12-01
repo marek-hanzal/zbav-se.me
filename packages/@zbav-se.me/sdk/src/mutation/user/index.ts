3d6043febbc313ad11d0c2e47354bf56f7befdbd
Marek Hanzal <marek.hanzal@icloud.com>
2025-12-01 19:01:11 +0100

    Add withFeedGalleryCreateMutation to SDK for gallery creation

--- packages/@zbav-se.me/sdk/src/mutation/user/index.ts
+++ packages/@zbav-se.me/sdk/src/mutation/user/index.ts
@@ -1,5 +1,6 @@
 export { withFeedCreateMutation } from "./withFeedCreateMutation";
 export { withFeedDeleteMutation } from "./withFeedDeleteMutation";
+export { withFeedGalleryCreateMutation } from "./withFeedGalleryCreateMutation";
 export { withFeedPatchMutation } from "./withFeedPatchMutation";
 export { withListingCartToggleMutation } from "./withListingCartToggleMutation";
 export { withListingCreateMutation } from "./withListingCreateMutation";
