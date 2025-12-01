8e72bed92e6ac5a40b5534a8cdffbb160c1a9bfb
Marek Hanzal <marek.hanzal@icloud.com>
2025-12-01 13:20:20 +0100

    Refactor ListingDetailContainer for improved layout and styling
    - Introduced VariantProvider for better theming and styling consistency.
    - Updated layout structure to enhance visual organization and responsiveness.
    - Improved BadgeValue components for better data presentation and user interaction.
    - Streamlined the BottomSheet integration for displaying listing metrics.

--- packages/@zbav-se.me/common/src/listing/ListingDetailContainer.tsx
+++ packages/@zbav-se.me/common/src/listing/ListingDetailContainer.tsx
@@ -2,11 +2,13 @@ import { ArrowRightIcon, Icon, ShowIcon, SpinnerIcon } from "@use-pico/client/ic
 import { BadgeValue } from "@use-pico/client/ui/badge";
 import { BottomSheet } from "@use-pico/client/ui/bottom-sheet";
 import { Container, SpinnerContainer } from "@use-pico/client/ui/container";
+import { VariantProvider } from "@use-pico/cls";
 import { toLocaleNumber } from "@use-pico/common/to-locale-number";
 import { CategoryInline } from "@zbav-se.me/common/category";
 import type { tGalleryItem, tListing } from "@zbav-se.me/sdk/api/user";
 import { withListingScoreCreateMutation } from "@zbav-se.me/sdk/mutation/user";
 import { withListingMetricsFetchQuery } from "@zbav-se.me/sdk/query/user";
+import { ThemeCls } from "@zbav-se.me/ui/cls";
 import { HeroImage } from "@zbav-se.me/ui/img";
 import { type FC, useEffect, useState } from "react";
 import { ScoreContainer } from "./ScoreContainer";
@@ -64,143 +66,169 @@ export const ListingDetailContainer: FC<ListingDetailContainer.Props> = ({
 	const [score, setScore] = useState<boolean>(false);
 
 	return (
-		<Container
-			layout={"vertical-content-footer"}
-			gap={"xl"}
-			height={"content"}
-			{...props}
+		<VariantProvider
+			cls={ThemeCls}
+			variant={{
+				tone: "unset",
+				theme: "unset",
+			}}
 		>
 			<Container
-				layout={"vertical-flex"}
-				gap={"sm"}
-			>
-				<Container
-					height={"content"}
-					round={"default"}
-				>
-					<HeroImage
-						src={hero.upload.url}
-						alt={`Hero image for listing ${listing.id}`}
-					/>
-				</Container>
-
-				<Container
-					layout={"vertical-flex"}
-					gap={"sm"}
-					square={"md"}
-					height={"content"}
-				>
-					<BadgeValue
-						textLabel={"Listing title (label)"}
-						textValue={listing.title}
-						textValueProps={{
-							truncate: false,
-							wrap: "wrap",
-						}}
-					/>
-
-					<BadgeValue
-						textLabel={"Listing price (label)"}
-						textValue={toLocaleNumber({
-							locale,
-							number: listing.price,
-							currency: listing.currency,
-							currencyDisplay: "narrowSymbol",
-							style: "currency",
-						})}
-					/>
-
-					<BadgeValue
-						textLabel={"Listing score hint (label)"}
-						textValue={
-							<withListingMetricsFetchQuery.Suspense
-								data={listing.id}
-								fallback={<Icon icon={SpinnerIcon} />}
-							>
-								{({ data }) => {
-									return toLocaleNumber({
-										locale,
-										number: data.score,
-										empty: "0",
-									});
-								}}
-							</withListingMetricsFetchQuery.Suspense>
-						}
-						action={
-							<Icon
-								icon={ShowIcon}
-								size={"sm"}
-							/>
-						}
-						onClick={() => setScore(true)}
-					/>
-
-					<BadgeValue
-						textLabel={"Listing seller hint (label)"}
-						textValue={"- skore + link -"}
-						action={<Icon icon={ArrowRightIcon} />}
-					/>
-
-					<BadgeValue
-						textLabel={"Listing location (label)"}
-						textValue={listing.location.address}
-						textValueProps={{
-							truncate: false,
-							wrap: "wrap",
-						}}
-					/>
-
-					<BadgeValue
-						textLabel={"Listing condition (label)"}
-						textValue={`Condition - Overall [${listing.condition}] (hint)`}
-					/>
-
-					<BadgeValue
-						textLabel={"Listing age (label)"}
-						textValue={`Condition - Age [${listing.age}] (hint)`}
-					/>
-
-					<BadgeValue
-						textLabel={"Listing category (label)"}
-						textValue={
-							<CategoryInline
-								category={listing.category}
-								tone="secondary"
-								theme="light"
-							/>
-						}
-					/>
-				</Container>
-			</Container>
-
-			<Container
-				square={"md"}
+				ui={"ListingDetailContainer-root"}
+				layout={children ? "vertical-content-footer" : undefined}
+				gap={"xl"}
 				height={"content"}
+				{...props}
 			>
-				{children}
-			</Container>
-
-			<BottomSheet
-				isOpen={score}
-				onClose={() => setScore(false)}
-				modalEffectRootId={parentSheetId}
-			>
-				<withListingMetricsFetchQuery.Suspense
-					data={listing.id}
-					fallback={<SpinnerContainer />}
+				<Container
+					ui={"ListingDetailContainer-layout"}
+					layout={"vertical-header-content"}
+					gap={"sm"}
+					height={"content"}
 				>
-					{({ data }) => {
-						return (
-							<Container square={"md"}>
-								<ScoreContainer
-									locale={locale}
-									listingMetrics={data}
-								/>
-							</Container>
-						);
-					}}
-				</withListingMetricsFetchQuery.Suspense>
-			</BottomSheet>
-		</Container>
+					<Container
+						ui={"ListingDetailContainer-image"}
+						height={"content"}
+						round={"default"}
+					>
+						<HeroImage
+							src={hero.upload.url}
+							alt={`Hero image for listing ${listing.id}`}
+						/>
+					</Container>
+
+					<VariantProvider
+						cls={ThemeCls}
+						variant={{
+							tone: "primary",
+							theme: "light",
+						}}
+					>
+						<Container
+							ui={"ListingDetailContainer-info"}
+							layout={"vertical-flex"}
+							gap={"sm"}
+							square={"md"}
+							tone={"unset"}
+							theme={"unset"}
+							height={"content"}
+						>
+							<BadgeValue
+								textLabel={"Listing title (label)"}
+								textValue={listing.title}
+								textValueProps={{
+									truncate: false,
+									wrap: "wrap",
+								}}
+							/>
+
+							<BadgeValue
+								textLabel={"Listing price (label)"}
+								textValue={toLocaleNumber({
+									locale,
+									number: listing.price,
+									currency: listing.currency,
+									currencyDisplay: "narrowSymbol",
+									style: "currency",
+								})}
+							/>
+
+							<BadgeValue
+								textLabel={"Listing score hint (label)"}
+								textValue={
+									<withListingMetricsFetchQuery.Suspense
+										data={listing.id}
+										fallback={<Icon icon={SpinnerIcon} />}
+									>
+										{({ data }) => {
+											return toLocaleNumber({
+												locale,
+												number: data.score,
+												empty: "0",
+											});
+										}}
+									</withListingMetricsFetchQuery.Suspense>
+								}
+								action={
+									<Icon
+										icon={ShowIcon}
+										size={"sm"}
+									/>
+								}
+								onClick={() => setScore(true)}
+							/>
+
+							<BadgeValue
+								textLabel={"Listing seller hint (label)"}
+								textValue={"- skore + link -"}
+								action={<Icon icon={ArrowRightIcon} />}
+							/>
+
+							<BadgeValue
+								textLabel={"Listing location (label)"}
+								textValue={listing.location.address}
+								textValueProps={{
+									truncate: false,
+									wrap: "wrap",
+								}}
+							/>
+
+							<BadgeValue
+								textLabel={"Listing condition (label)"}
+								textValue={`Condition - Overall [${listing.condition}] (hint)`}
+							/>
+
+							<BadgeValue
+								textLabel={"Listing age (label)"}
+								textValue={`Condition - Age [${listing.age}] (hint)`}
+							/>
+
+							<BadgeValue
+								textLabel={"Listing category (label)"}
+								textValue={
+									<CategoryInline
+										category={listing.category}
+										tone="secondary"
+										theme="light"
+									/>
+								}
+							/>
+						</Container>
+					</VariantProvider>
+				</Container>
+
+				{children ? (
+					<Container
+						ui={"ListingDetailContainer-content"}
+						square={"md"}
+						height={"content"}
+					>
+						{children}
+					</Container>
+				) : null}
+
+				<BottomSheet
+					isOpen={score}
+					onClose={() => setScore(false)}
+					modalEffectRootId={parentSheetId}
+				>
+					<withListingMetricsFetchQuery.Suspense
+						data={listing.id}
+						fallback={<SpinnerContainer />}
+					>
+						{({ data }) => {
+							return (
+								<Container square={"md"}>
+									<ScoreContainer
+										locale={locale}
+										listingMetrics={data}
+									/>
+								</Container>
+							);
+						}}
+					</withListingMetricsFetchQuery.Suspense>
+				</BottomSheet>
+			</Container>
+		</VariantProvider>
 	);
 };
