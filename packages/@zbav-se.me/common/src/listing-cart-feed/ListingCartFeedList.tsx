ef1100e4136f777d1467740930f7b96ffd6fd277
Marek Hanzal <marek.hanzal@icloud.com>
2025-12-04 13:27:24 +0100

    Add empty cart state handling in ListingCartFeedList component
    - Implemented a new status display for when the cart is empty, including a message and a button to navigate to listings.
    - Enhanced user experience by providing clear guidance and action options when no items are present in the cart.

--- packages/@zbav-se.me/common/src/listing-cart-feed/ListingCartFeedList.tsx
+++ packages/@zbav-se.me/common/src/listing-cart-feed/ListingCartFeedList.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@use-pico/client/ui/button";
 import { Container, SpinnerContainer } from "@use-pico/client/ui/container";
+import { LinkTo } from "@use-pico/client/ui/link-to";
+import { Status } from "@use-pico/client/ui/status";
 import type { tFeedQuery } from "@zbav-se.me/sdk/api/user";
 import { withListingCartFeedCollectionQuery } from "@zbav-se.me/sdk/query/user";
+import { CartIcon } from "@zbav-se.me/ui/icon";
 import type { FC } from "react";
 import { FeedItemBadge } from "../feed";
 
@@ -28,6 +32,27 @@ export const ListingCartFeedList: FC<ListingCartFeedList.Props> = ({
 				fallback={<SpinnerContainer />}
 			>
 				{({ data }) => {
+					if (data.data.length === 0) {
+						return (
+							<Container
+								layout={"vertical-centered"}
+								items={"center"}
+							>
+								<Status
+									icon={CartIcon}
+									textTitle={"No items in cart (title)"}
+									action={
+										<>
+											<LinkTo to={"feedef"}>
+												<Button label={"Go to listings (button)"} />
+											</LinkTo>
+										</>
+									}
+								/>
+							</Container>
+						);
+					}
+
 					return (
 						<Container
 							ui={"ListingCartFeedList-root"}
