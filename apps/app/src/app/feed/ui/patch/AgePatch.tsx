5d6350add82b20cd0acfaed85d237285e07e285e
Marek Hanzal <marek.hanzal@icloud.com>
2025-12-13 21:01:50 +0100

    Integrate uiSaveButton in AgePatch for enhanced button functionality
    - Updated AgePatch component to utilize uiSaveButton, improving button styling and functionality with icon support.
    - Adjusted layout properties for better alignment and responsiveness.
    - Removed deprecated location security hints from translation files for cleaner codebase.

--- apps/app/src/app/feed/ui/patch/AgePatch.tsx
+++ apps/app/src/app/feed/ui/patch/AgePatch.tsx
@@ -1,10 +1,12 @@
 import { useSelection } from "@use-pico/client/hook";
+import { SaveIcon } from "@use-pico/client/icon";
 import { Button } from "@use-pico/client/ui/button";
 import { Container } from "@use-pico/client/ui/container";
 import { translator } from "@use-pico/common/translator";
 import type { tFeed } from "@zbav-se.me/sdk/api/user";
 import { withFeedPatchMutation } from "@zbav-se.me/sdk/mutation/user";
 import type { Rating } from "@zbav-se.me/ui/rating";
+import { uiSaveButton } from "@zbav-se.me/ui/ui";
 import { type FC, useState } from "react";
 import { toast } from "sonner";
 import { AgeSelection } from "~/app/age/ui/AgeSelection";
@@ -43,6 +45,7 @@ export const AgePatch: FC<AgePatch.Props> = ({ feed, onSettled, ...props }) => {
 				layout: "vertical-content-footer",
 				height: "full",
 				gap: "default",
+				inner: "default",
 			}}
 			{...props}
 		>
@@ -52,6 +55,12 @@ export const AgePatch: FC<AgePatch.Props> = ({ feed, onSettled, ...props }) => {
 				label={"Feed - save (button)"}
 				loading={mutation.isPending}
 				disabled={!change || mutation.isPending}
+				iconEnabled={SaveIcon}
+				iconProps={{
+					ui: {
+						text: "2xl",
+					},
+				}}
 				onClick={() => {
 					toast.promise(
 						mutation.mutateAsync({
@@ -80,11 +89,9 @@ export const AgePatch: FC<AgePatch.Props> = ({ feed, onSettled, ...props }) => {
 						},
 					);
 				}}
-				ui={{
-					tone: "secondary",
-					theme: "dark",
-					size: "xl",
-				}}
+				{...uiSaveButton({
+					className: [],
+				})}
 			/>
 		</Container>
 	);
