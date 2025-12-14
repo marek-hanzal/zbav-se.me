1f3fbf36d289bb9103b9e1e8398144be865fda5c
Marek Hanzal <marek.hanzal@icloud.com>
2025-12-14 13:48:32 +0100

    Add migrations for feed and user_ex tables
    - Created migration files for the 'feed' and 'user_ex' tables, defining necessary columns and constraints.
    - Implemented foreign key constraints to maintain data integrity with related tables.
    - Added unique constraints and indexes for optimized query performance on relevant columns.

---
+++ apps/server/src/database/migrations/0014-user-ex.ts
@@ -0,0 +1,52 @@
+import { type Migration, sql } from "kysely";
+
+export const UserExMigration: Migration = {
+	async up(db) {
+		await db.schema
+			.createType("user_ex_side_enum")
+			.asEnum([
+				"seller",
+				"buyer",
+			])
+			.execute();
+
+		await db.schema
+			.createTable("user_ex")
+			.addColumn("id", "text", (col) => col.primaryKey().notNull())
+			.addColumn("userId", "text", (col) => col.notNull().unique())
+			.addColumn("locationId", "text")
+			.addColumn("side", sql`user_ex_side_enum`)
+			.addForeignKeyConstraint(
+				"user_ex_[userId]_fk",
+				[
+					"userId",
+				],
+				"user",
+				[
+					"id",
+				],
+				(c) => c.onDelete("cascade"),
+			)
+			.addForeignKeyConstraint(
+				"user_ex_[locationId]_fk",
+				[
+					"locationId",
+				],
+				"location",
+				[
+					"id",
+				],
+				(c) => c.onDelete("cascade"),
+			)
+			.addUniqueConstraint("user_ex_[userId]_unique_idx", [
+				"userId",
+			])
+			.execute();
+
+		await db.schema
+			.createIndex("user_ex_[locationId]_idx")
+			.on("user_ex")
+			.column("locationId")
+			.execute();
+	},
+};
