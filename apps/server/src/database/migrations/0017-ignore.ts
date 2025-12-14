36a8a9233be41a5e400d3a5e2349446e893b14c9
Marek Hanzal <marek.hanzal@icloud.com>
2025-12-14 14:07:48 +0100

    Add migration for ignore table
    - Created a new migration file to establish the 'ignore' table in the database.
    - Defined columns for id, userId, listingId, and createdAt, ensuring all are not null.
    - Implemented foreign key constraints for userId and listingId to maintain data integrity.
    - Added unique constraints and indexes for optimized query performance on relevant columns.

---
+++ apps/server/src/database/migrations/0017-ignore.ts
@@ -0,0 +1,53 @@
+import type { Migration } from "kysely";
+
+export const IgnoreMigration: Migration = {
+	async up(db) {
+		await db.schema
+			.createTable("ignore")
+			.addColumn("id", "text", (col) => col.primaryKey().notNull())
+			.addColumn("userId", "text", (col) => col.notNull())
+			.addColumn("listingId", "text", (col) => col.notNull())
+			.addColumn("createdAt", "timestamp", (col) => col.notNull().defaultTo("now()"))
+			.addForeignKeyConstraint(
+				"ignore_[userId]_fk",
+				[
+					"userId",
+				],
+				"user",
+				[
+					"id",
+				],
+				(c) => c.onDelete("cascade"),
+			)
+			.addForeignKeyConstraint(
+				"ignore_[listingId]_fk",
+				[
+					"listingId",
+				],
+				"listing",
+				[
+					"id",
+				],
+				(c) => c.onDelete("cascade"),
+			)
+			.addUniqueConstraint("ignore_[userId-listingId]_unique_idx", [
+				"userId",
+				"listingId",
+			])
+			.execute();
+
+		await db.schema.createIndex("ignore_[userId]_idx").on("ignore").column("userId").execute();
+
+		await db.schema
+			.createIndex("ignore_[listingId]_idx")
+			.on("ignore")
+			.column("listingId")
+			.execute();
+
+		await db.schema
+			.createIndex("ignore_[createdAt]_idx")
+			.on("ignore")
+			.column("createdAt")
+			.execute();
+	},
+};
