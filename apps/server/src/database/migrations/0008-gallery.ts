78bff59863fe7eee5febbeff7b5f3ba14b124034
Marek Hanzal <marek.hanzal@icloud.com>
2025-12-14 13:44:16 +0100

    Add gallery and gallery_item migrations with foreign key constraints and indexes
    - Created a new migration file for the 'gallery' table, including columns for id, userId, and createdAt, along with foreign key constraints to the user table.
    - Added indexes on userId and createdAt for optimized query performance.
    - Introduced a migration file for the 'gallery_item' table, featuring columns for id, galleryId, sort, uploadId, and createdAt, with foreign key constraints to both the gallery and upload tables.
    - Created indexes on galleryId and createdAt for improved query efficiency.

---
+++ apps/server/src/database/migrations/0008-gallery.ts
@@ -0,0 +1,36 @@
+import type { Migration } from "kysely";
+
+export const GalleryMigration: Migration = {
+	async up(db) {
+		await db.schema
+			.createTable("gallery")
+			.addColumn("id", "text", (col) => col.primaryKey().notNull())
+			//
+			.addColumn("userId", "text", (col) => col.notNull())
+			.addColumn("createdAt", "timestamp", (col) => col.notNull().defaultTo("now()"))
+			.addForeignKeyConstraint(
+				"gallery_[userId]_fk",
+				[
+					"userId",
+				],
+				"user",
+				[
+					"id",
+				],
+				(c) => c.onDelete("cascade"),
+			)
+			.execute();
+
+		await db.schema
+			.createIndex("gallery_[userId]_idx")
+			.on("gallery")
+			.column("userId")
+			.execute();
+
+		await db.schema
+			.createIndex("gallery_[createdAt]_idx")
+			.on("gallery")
+			.column("createdAt")
+			.execute();
+	},
+};
