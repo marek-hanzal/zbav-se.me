193378660df19d27d2a20b3a7142a24220e4b119
Marek Hanzal <marek.hanzal@icloud.com>
2025-12-23 19:17:09 +0100

    Refactor ListingDetail component to conditionally render delivery ValueList
    - Updated the ListingDetail component to conditionally render the ValueList for delivery options only when delivery data is available, improving the component's responsiveness and user experience.

--- apps/app/src/app/listing/ui/ListingDetail.tsx
+++ apps/app/src/app/listing/ui/ListingDetail.tsx
@@ -146,16 +146,18 @@ export const ListingDetail: FC<ListingDetail.Props> = ({
 					/>
 				) : null}
 
-				<ValueList
-					data-ui={"ListingDetail[DeliveryValue]"}
-					textLabel={translator.text("Listing delivery (label)")}
-					textEmpty={translator.text("Delivery not selected")}
-					items={(listing.delivery ?? []).map((delivery) => ({
-						id: delivery,
-						delivery,
-					}))}
-					renderFn={(item) => <Tx label={`Listing delivery - ${item.delivery}`} />}
-				/>
+				{listing.delivery?.length ? (
+					<ValueList
+						data-ui={"ListingDetail[DeliveryValue]"}
+						textLabel={translator.text("Listing delivery (label)")}
+						textEmpty={translator.text("Delivery not selected")}
+						items={(listing.delivery ?? []).map((delivery) => ({
+							id: delivery,
+							delivery,
+						}))}
+						renderFn={(item) => <Tx label={`Listing delivery - ${item.delivery}`} />}
+					/>
+				) : null}
 
 				{listing.pros?.length ? (
 					<ValueList
