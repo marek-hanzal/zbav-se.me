93fcf06a1765fd129de9edb18a18fe29c4893e4e
Marek Hanzal <marek.hanzal@icloud.com>
2025-12-24 00:06:58 +0100

    Add FeedbackLikeButton and FeedbackDislikeButton components
    - Implemented FeedbackLikeButton and FeedbackDislikeButton components for user feedback on listings.
    - Each button utilizes the feedback mutation functionality to submit 'like' and 'dislike' actions, respectively.
    - Enhanced user interaction by providing visual feedback during the mutation process with loading states and disabled states when mutating.

---
+++ apps/app/src/app/listing/ui/button/FeedbackDislikeButton.tsx
@@ -0,0 +1,53 @@
+import { CloseIcon } from "@use-pico/client/icon";
+import { Button } from "@use-pico/client/ui/button";
+import { withFeedbackCreateMutation } from "@zbav-se.me/sdk/mutation/user/feedback";
+import type { FC } from "react";
+
+export namespace FeedbackDislikeButton {
+	export interface Props extends Button.Props {
+		listingId: string;
+	}
+}
+
+export const FeedbackDislikeButton: FC<FeedbackDislikeButton.Props> = ({
+	listingId,
+	ui,
+	...props
+}) => {
+	const feedbackCreateMutation = withFeedbackCreateMutation.useMutation({
+		meta: {
+			mutationId: listingId,
+		},
+	});
+	const isMutating = withFeedbackCreateMutation.useIsMutating({
+		mutationId: listingId,
+	});
+
+	return (
+		<Button
+			label={"Dislike listing (button)"}
+			iconEnabled={CloseIcon}
+			iconProps={{
+				ui: {
+					text: "xl",
+				},
+			}}
+			disabled={isMutating}
+			loading={feedbackCreateMutation.isPending}
+			onClick={() => {
+				feedbackCreateMutation.mutate({
+					listingId,
+					type: "dislike",
+				});
+			}}
+			ui={{
+				tone: "primary",
+				theme: "light",
+				size: "default",
+				justify: "start",
+				...ui,
+			}}
+			{...props}
+		/>
+	);
+};
---
+++ apps/app/src/app/listing/ui/button/FeedbackLikeButton.tsx
@@ -0,0 +1,49 @@
+import { CheckIcon } from "@use-pico/client/icon";
+import { Button } from "@use-pico/client/ui/button";
+import { withFeedbackCreateMutation } from "@zbav-se.me/sdk/mutation/user/feedback";
+import type { FC } from "react";
+
+export namespace FeedbackLikeButton {
+	export interface Props extends Button.Props {
+		listingId: string;
+	}
+}
+
+export const FeedbackLikeButton: FC<FeedbackLikeButton.Props> = ({ listingId, ui, ...props }) => {
+	const feedbackCreateMutation = withFeedbackCreateMutation.useMutation({
+		meta: {
+			mutationId: listingId,
+		},
+	});
+	const isMutating = withFeedbackCreateMutation.useIsMutating({
+		mutationId: listingId,
+	});
+
+	return (
+		<Button
+			label={"Like listing (button)"}
+			iconEnabled={CheckIcon}
+			iconProps={{
+				ui: {
+					text: "xl",
+				},
+			}}
+			disabled={isMutating}
+			loading={feedbackCreateMutation.isPending}
+			onClick={() => {
+				feedbackCreateMutation.mutate({
+					listingId,
+					type: "like",
+				});
+			}}
+			ui={{
+				tone: "primary",
+				theme: "light",
+				size: "default",
+				justify: "start",
+				...ui,
+			}}
+			{...props}
+		/>
+	);
+};
