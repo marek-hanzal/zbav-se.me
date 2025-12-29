da888610ab6c0c017a3f53efbc9d359bc4725336
Marek Hanzal <marek.hanzal@icloud.com>
2025-12-29 11:26:58 +0100

    Update TransactionStatusEnumSchema to include new transaction states and improve documentation
    - Added new transaction states: "resolved", "expired", and "success" to better represent transaction lifecycle.
    - Enhanced code documentation with comments categorizing states into initial, running, and terminal for clarity.

--- apps/server/src/app/transaction/schema/ListingTransactionStatusEnumSchema.ts
+++ apps/server/src/app/transaction/schema/ListingTransactionStatusEnumSchema.ts
@@ -2,12 +2,32 @@ import { z } from "@hono/zod-openapi";
 
 export const TransactionStatusEnumSchema = z
 	.enum([
+		/**
+		 * Initial states
+		 */
+
+		// New, fresh transaction
 		"pending",
+
+		/**
+		 * Running (open) states
+		 */
+
+		// Accepted (running) transaction; all the stuff happens here
 		"open",
+		// Resolved by seller (only) - e.g. package sent
+		"resolved",
+
+		/**
+		 * Terminal states
+		 */
+
+		// Explicitly closed transaction by seller or buyer
 		"rejected",
-		"completed",
-		"cancelled",
+		// Expired transaction is "unknown" state (no action received from either side)
 		"expired",
+		// Probably rare one - submitted by buyer he's happy with the result
+		"success",
 	])
 	.openapi("TransactionStatusEnum", {
 		description: "Current status of the listing transaction",
