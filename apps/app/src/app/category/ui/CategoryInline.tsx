7f9e48014ee2b2782c57b5a5f4cd7f2f3fa00878
Marek Hanzal <marek.hanzal@icloud.com>
2026-01-20 18:53:39 +0100

    Refactor CategoryInline component to extend Container props, simplifying the usage of tone and theme by integrating them into the ui prop. Updated related components to reflect this change, enhancing maintainability and consistency across the codebase.

--- apps/app/src/app/category/ui/CategoryInline.tsx
+++ apps/app/src/app/category/ui/CategoryInline.tsx
@@ -1,16 +1,13 @@
+import { Container } from "@use-pico/client/ui/container";
 import { Typo } from "@use-pico/client/ui/typo";
-import { type Cls, VariantProvider } from "@use-pico/cls";
 import type { tCategory } from "@zbav-se.me/sdk/api/session";
-import { ThemeCls } from "@zbav-se.me/ui/cls";
 import type { FC } from "react";
 
 export namespace CategoryInline {
-	export interface Props {
+	export interface Props extends Container.Props {
 		category: Pick<tCategory, "group" | "category">;
 		textGroupProps?: Typo.PropsEx;
 		textCategoryProps?: Typo.PropsEx;
-		tone?: Cls.VariantOf<ThemeCls, "tone">;
-		theme?: Cls.VariantOf<ThemeCls, "theme">;
 	}
 }
 
@@ -18,38 +15,36 @@ export const CategoryInline: FC<CategoryInline.Props> = ({
 	category,
 	textGroupProps,
 	textCategoryProps,
-	tone,
-	theme,
+	...props
 }) => {
 	return (
-		<VariantProvider
-			cls={ThemeCls}
-			variant={{
-				tone,
-				theme,
+		<Container
+			ui={{
+				flow: "vertical",
+				gap: "xs",
+				items: "start",
 			}}
+			{...props}
 		>
-			<div className={"flex flex-col gap-0.5 items-start"}>
-				<Typo
-					label={category.group}
-					ui={{
-						tone: "secondary",
-						theme: "light",
-						text: "xs",
-					}}
-					{...textGroupProps}
-				/>
+			<Typo
+				label={category.group}
+				ui={{
+					tone: "secondary",
+					theme: "light",
+					text: "xs",
+				}}
+				{...textGroupProps}
+			/>
 
-				<Typo
-					label={category.category}
-					ui={{
-						tone: "secondary",
-						theme: "light",
-						text: "default",
-					}}
-					{...textCategoryProps}
-				/>
-			</div>
-		</VariantProvider>
+			<Typo
+				label={category.category}
+				ui={{
+					tone: "secondary",
+					theme: "light",
+					text: "default",
+				}}
+				{...textCategoryProps}
+			/>
+		</Container>
 	);
 };
