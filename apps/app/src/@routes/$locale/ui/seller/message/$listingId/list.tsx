1bf39f5063499900676783f858805f8275fa497e
Marek Hanzal <marek.hanzal@icloud.com>
2026-01-21 14:03:14 +0100

    Implement seller message listing component with enhanced loading state and transaction display
    - Added a pending component for loading states using TitleContainer and SpinnerContainer.
    - Integrated withListingFetchQuery to fetch listing data and display the title in the message listing.
    - Enhanced the component structure to include TransactionList for displaying transactions related to the listing.
    - Improved navigation with LinkTo component for better user experience.

--- apps/app/src/@routes/$locale/ui/seller/message/$listingId/list.tsx
+++ apps/app/src/@routes/$locale/ui/seller/message/$listingId/list.tsx
@@ -1,7 +1,81 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { ArrowLeftIcon } from "@use-pico/client/icon";
+import { SpinnerContainer } from "@use-pico/client/ui/container";
+import { LinkTo } from "@use-pico/client/ui/link-to";
+import { withListingFetchQuery } from "@zbav-se.me/sdk/query/user/listing";
+import { TitleContainer } from "@zbav-se.me/ui/container";
+import { TransactionList } from "~/app/transaction/ui/TransactionList";
 
 export const Route = createFileRoute("/$locale/ui/seller/message/$listingId/list")({
+	pendingComponent() {
+		const { locale } = Route.useParams();
+
+		return (
+			<TitleContainer
+				data-ui="/seller/message/list[TitleContainer]"
+				textTitle={"Messages (title)"}
+				left={
+					<LinkTo
+						icon={ArrowLeftIcon}
+						to="/$locale/ui/seller/message/list"
+						params={{
+							locale,
+						}}
+					/>
+				}
+			>
+				<SpinnerContainer />
+			</TitleContainer>
+		);
+	},
 	component() {
-		return "bello!";
+		const { listingId } = Route.useParams();
+		const { locale } = Route.useParams();
+		const { data: listing } = withListingFetchQuery.useSuspenseQuery({
+			where: {
+				id: listingId,
+			},
+		});
+
+		return (
+			<TitleContainer
+				data-ui="/seller/message/list[TitleContainer]"
+				textTitle={"Messages (title)"}
+				textSubtitle={listing.title}
+				left={
+					<LinkTo
+						icon={ArrowLeftIcon}
+						to="/$locale/ui/seller/message/list"
+						params={{
+							locale,
+						}}
+					/>
+				}
+			>
+				<TransactionList
+					query={{
+						where: {
+							listingId,
+						},
+						sort: [
+							{
+								field: "status",
+								direction: "asc",
+							},
+							{
+								field: "createdAt",
+								direction: "desc",
+							},
+						],
+						meta: {
+							side: "seller",
+						},
+					}}
+					ui={{
+						inner: "default",
+					}}
+				/>
+			</TitleContainer>
+		);
 	},
 });
