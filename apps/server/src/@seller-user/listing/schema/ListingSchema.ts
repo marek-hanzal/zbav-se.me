2bcb4793ea4a936fcbdd3b7ff953b7d40147eaa3
Marek Hanzal <marek.hanzal@icloud.com>
2026-01-28 18:20:30 +0100

    Update ListingSchema import and introduce new ListingSchema for seller users
    - Updated the import path for `ListingSchema` to align with the `@seller-user` namespace, enhancing code organization.
    - Created a new `ListingSchema` file to define the structure and validation for listing data, improving the API's capabilities for seller users.
    - Ensured consistency in schema definitions to promote maintainability across the application.

---
+++ apps/server/src/@seller-user/listing/schema/ListingSchema.ts
@@ -0,0 +1,60 @@
+import { z } from "@hono/zod-openapi";
+import { ThumbEnumSchema } from "~/database/@enum/ThumbEnumSchema";
+import { CategorySchema } from "~/@session/category/schema/CategorySchema";
+import { LocationSchema } from "~/@session/location/schema/LocationSchema";
+import { GallerySchema } from "~/@user/gallery/schema/GallerySchema";
+import { ListingTableSchema } from "~/database/@table/ListingTableSchema";
+
+export const ListingSchema = z
+	.looseObject({
+		...ListingTableSchema.shape,
+		location: LocationSchema,
+		category: CategorySchema,
+		distance: z.number().nullable().openapi({
+			description:
+				"Distance from the input location to the listing (in km; meta lat/lon must be provided)",
+		}),
+		gallery: GallerySchema.openapi({
+			description: "Listing gallery images",
+		}),
+		isFavourite: z.boolean().openapi({
+			description: "Whether the user has this listing in favourites",
+		}),
+		isIgnored: z.boolean().openapi({
+			description: "Whether the user ignored this listing",
+		}),
+		hasFlag: z.boolean().openapi({
+			description: "Whether the user flagged this listing",
+		}),
+		transactionId: z
+			.union([
+				z.string(),
+				z.null(),
+			])
+			.openapi({
+				description: "Whether the user has a transaction with this listing",
+			}),
+		thumb: z
+			.union([
+				ThumbEnumSchema,
+				z.null(),
+			])
+			.openapi({
+				description:
+					"Thumb type provided by the user (like/dislike) or null if not present",
+			}),
+	})
+	.omit({
+		userId: true,
+		titleVec: true,
+	})
+	.strip()
+	.openapi("Listing", {
+		description: "Listing data",
+	});
+
+export type ListingSchema = typeof ListingSchema;
+
+export namespace ListingSchema {
+	export type Type = z.infer<ListingSchema>;
+}
