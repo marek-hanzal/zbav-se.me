da5f873e6c8ab6a4be2f5709eba877bae2c309ca
Marek Hanzal <marek.hanzal@icloud.com>
2026-02-09 15:08:16 +0100

    Refactor error handling in tryDbFx to remove mapToError utility
    - Removed the `mapToError` utility and integrated its functionality directly into the `tryDbFx` effect. This change simplifies error handling by directly managing database errors within the effect, improving code clarity and maintainability.
    - Updated the input structure of `tryDbFx` to accept a `run` function and an optional `conflict` message, enhancing its usability.

--- apps/server/src/database/fx/tryDbFx.ts
+++ apps/server/src/database/fx/tryDbFx.ts
@@ -1,12 +1,46 @@
 import { Effect } from "effect";
-import { mapToError } from "~/database/mapToError";
+import { DatabaseError } from "pg";
+import { ConflictErrorFx } from "~/error/ConflictErrorFx";
+import { RuntimeErrorFx } from "~/error/RuntimeErrorFx";
 
-export const tryDbFx = Effect.fn("tryDbFx")(function* <TResult>(
-	promise: () => Promise<TResult>,
-	error: mapToError.Props,
-) {
+export namespace tryDbFx {
+	export interface Props<TResult> {
+		run(): Promise<TResult>;
+		conflict?: string;
+	}
+}
+
+export const tryDbFx = Effect.fn("tryDbFx")(function* <TResult>({
+	run,
+	conflict,
+}: tryDbFx.Props<TResult>) {
 	return yield* Effect.tryPromise({
-		try: promise,
-		catch: mapToError(error),
+		try: run,
+		catch(error: unknown) {
+			if (error instanceof DatabaseError) {
+				switch (error.code) {
+					case "23505":
+						return new ConflictErrorFx({
+							message: conflict ?? "(unknown conflict)",
+							cause: error,
+						});
+				}
+
+				return new RuntimeErrorFx({
+					message: error.message,
+					cause: error,
+				});
+			} else if (error instanceof Error) {
+				return new RuntimeErrorFx({
+					message: error.message,
+					cause: error,
+				});
+			}
+
+			return new RuntimeErrorFx({
+				message: String(error),
+				cause: error,
+			});
+		},
 	});
 });
