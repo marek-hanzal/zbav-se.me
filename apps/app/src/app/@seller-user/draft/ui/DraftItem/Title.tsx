204273005e61f2c1677fec681b1e2ed64b018bb9
Marek Hanzal <marek.hanzal@icloud.com>
2026-02-27 19:35:05 +0100

    refactor(ui): update Title component to accept string or null for title prop
    - Changed the title prop type in the Title component from tDraft["title"] to string | null for improved flexibility in handling title values.

--- apps/app/src/app/@seller-user/draft/ui/DraftItem/Title.tsx
+++ apps/app/src/app/@seller-user/draft/ui/DraftItem/Title.tsx
@@ -1,10 +1,9 @@
 import { Tx } from "@use-pico/client/ui/tx";
-import type { tDraft } from "@zbav-se.me/sdk/api/seller-user";
 import type { FC } from "react";
 
 export namespace Title {
 	export interface Props {
-		title: tDraft["title"];
+		title: string | null;
 	}
 }
 
