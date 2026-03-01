5e31d9ff64d8e9715036a8766a530d6947a26cb5
Marek Hanzal <marek.hanzal@icloud.com>
2026-03-01 14:40:37 +0100

    refactor(listing): restructure Data component for improved clarity and modularity
    - Deleted the old Data.tsx file and replaced it with a new Data component that utilizes separate Empty and FilterEmpty components for better organization.
    - Introduced Empty and FilterEmpty components to handle specific states when no listings are available or when filters yield no results, enhancing code readability and maintainability.
    - Updated index file to reflect the new structure, ensuring easier imports and exports within the ListingListContainer.

---
+++ apps/app/src/app/v0/@seller-user/listing/ui/ListingListContainer/Data/Empty.tsx
@@ -0,0 +1,55 @@
+import { useLocale } from "@use-pico/client/hook";
+import { ChevronRightIcon } from "@use-pico/client/icon";
+import { Container } from "@use-pico/client/ui/container";
+import { LinkTo } from "@use-pico/client/ui/link-to";
+import { Status } from "@use-pico/client/ui/status";
+import { Tx } from "@use-pico/client/ui/tx";
+import { translator } from "@use-pico/common/translator";
+import { SearchIcon } from "@zbav-se.me/ui/icon";
+import type { FC } from "react";
+
+export const Empty: FC = () => {
+	const locale = useLocale();
+
+	return (
+		<Container
+			ui={{
+				layout: "vertical-centered",
+				height: "full",
+			}}
+		>
+			<Status
+				data-ui={"MyListing-[Status.empty]"}
+				icon={SearchIcon}
+				textTitle={translator.text("No my listings (title)")}
+				textMessage={translator.text("No my listings (message)")}
+				action={
+					<LinkTo
+						icon={ChevronRightIcon}
+						iconPosition={"right"}
+						to={"/$locale/seller/draft/resolve"}
+						params={{
+							locale,
+						}}
+						ui={{
+							background: "default",
+							border: true,
+							shadow: true,
+							round: "default",
+							size: "default",
+						}}
+					>
+						<Tx label={"Create listing (label)"} />
+					</LinkTo>
+				}
+				ui={{
+					tone: "brand",
+					theme: "light",
+					color: "lead",
+					inner: "4xl",
+				}}
+				className="text-center"
+			/>
+		</Container>
+	);
+};
