555c9b4eeb466411505b756686ec26c0476eb46d
Marek Hanzal <marek.hanzal@icloud.com>
2026-03-15 16:12:43 +0100

    Implement EmptyState handling in TransactionList and introduce Empty component
    - Added an Empty component to provide user feedback when no transactions are available.
    - Updated the Data component to utilize EmptyState for improved handling of empty transaction collections.
    - Refactored the rendering logic to enhance user experience and maintainability.

---
+++ apps/app/src/app/@buyer/transaction/ui/TransactionList/Empty.tsx
@@ -0,0 +1,61 @@
+import { useLocale } from "@use-pico/client/hook";
+import { ChevronRightIcon, MessageIcon } from "@use-pico/client/icon";
+import { Container } from "@use-pico/client/ui/container";
+import { LinkTo } from "@use-pico/client/ui/link-to";
+import { Status } from "@use-pico/client/ui/status";
+import { Tx } from "@use-pico/client/ui/tx";
+import { translator } from "@use-pico/common/translator";
+import type { FC } from "react";
+
+export namespace Empty {
+	export interface Props extends Container.Props {
+		//
+	}
+}
+
+export const Empty: FC<Empty.Props> = ({ ui, ...props }) => {
+	const locale = useLocale();
+
+	return (
+		<Container
+			ui={{
+				layout: "vertical-centered",
+				height: "full",
+				...ui,
+			}}
+			{...props}
+		>
+			<Status
+				icon={MessageIcon}
+				textTitle={translator.text("No transactions as buyer (title)")}
+				textMessage={translator.text("No transactions as buyer (message)")}
+				action={
+					<LinkTo
+						icon={ChevronRightIcon}
+						iconPosition={"right"}
+						to="/$locale/buyer/feed/default"
+						params={{
+							locale,
+						}}
+						ui={{
+							background: "default",
+							border: true,
+							shadow: true,
+							round: "default",
+							size: "default",
+						}}
+					>
+						<Tx label="Go to my feed (button)" />
+					</LinkTo>
+				}
+				ui={{
+					tone: "brand",
+					theme: "light",
+					color: "lead",
+					inner: "4xl",
+				}}
+				className={"text-center"}
+			/>
+		</Container>
+	);
+};
