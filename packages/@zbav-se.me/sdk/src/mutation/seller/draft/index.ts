793adce3d1822d23633e8073dff70d35d4f2b3d1
Marek Hanzal <marek.hanzal@icloud.com>
2026-03-26 11:36:39 +0100

    Remove deprecated draft mutation files and update exports
    - Deleted the withDraftCreateMutation, withDraftDeleteMutation, and withDraftPatchMutation files to streamline the SDK and remove unused functionalities.
    - Updated the index.ts file to reflect the removal of these mutations, ensuring the codebase remains clean and maintainable.

--- packages/@zbav-se.me/sdk/src/mutation/seller/draft/index.ts
+++ packages/@zbav-se.me/sdk/src/mutation/seller/draft/index.ts
@@ -1,4 +1,1 @@
-export { withDraftCreateMutation } from "./withDraftCreateMutation";
-export { withDraftDeleteMutation } from "./withDraftDeleteMutation";
 export { withDraftGalleryCreateMutation } from "./withDraftGalleryCreateMutation";
-export { withDraftPatchMutation } from "./withDraftPatchMutation";
