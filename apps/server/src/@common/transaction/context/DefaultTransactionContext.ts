0105783be8c2938d8a100ecc106cea057d4acf5f
Marek Hanzal <marek.hanzal@icloud.com>
2026-03-26 20:26:19 +0100

    Remove unused API functionalities and schemas
    - Deleted multiple files related to S3 context and transaction management, including `S3ContextFx.ts`, `withS3Fx.ts`, `DefaultTransactionContext.ts`, and various transaction-related schemas.
    - This removal is part of an effort to streamline the codebase and focus on more relevant functionalities, enhancing maintainability and reducing complexity in the S3 and transaction management systems.

--- apps/server/src/@common/transaction/context/DefaultTransactionContext.ts
+++
@@ -1,6 +0,0 @@
-import type { TransactionContext } from "~/@common/transaction/context/TransactionContextFx";
-
-export const DefaultTransactionContext: TransactionContext = {
-	expires: 3,
-	extend: 3,
-};
--- apps/server/src/@common/upload/context/withUploadFx.ts
+++
@@ -1,7 +0,0 @@
-import { Effect } from "effect";
-import { type UploadContext, UploadContextFx } from "~/@common/upload/context/UploadContextFx";
-
-export const withUploadFx =
-	(context: UploadContext) =>
-	<A, E, R>(eff: Effect.Effect<A, E, R>) =>
-		eff.pipe(Effect.provideService(UploadContextFx, context));
