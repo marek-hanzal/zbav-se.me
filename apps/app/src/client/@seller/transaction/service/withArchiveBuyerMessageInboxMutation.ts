50adc00805c740d8a50c742284a0695cb63397b9
Marek Hanzal <marek.hanzal@icloud.com>
2026-03-26 15:40:55 +0100

    Refactor inbox archiving logic to use new function
    - Replaced the deprecated `apiInboxArchive` with `inboxArchiveFn` for archiving buyer messages, enhancing code clarity and maintainability.
    - Updated the response types to improve error handling consistency.
    - These changes align with recent refactoring efforts to streamline inbox functionalities.

--- apps/app/src/client/@seller/transaction/service/withArchiveBuyerMessageInboxMutation.ts
+++ apps/app/src/client/@seller/transaction/service/withArchiveBuyerMessageInboxMutation.ts
@@ -1,16 +1,9 @@
 import { withMutation } from "@use-pico/client/mutation";
-import { withApi } from "@use-pico/common/api";
 import { tTransactionStatusEnum } from "@zbav-se.me/sdk/api/seller";
-import {
-	apiInboxArchive,
-	type apiInboxArchiveError,
-	type tApiInboxArchiveResponse,
-	tInboxFamilyEnum,
-	tInboxTypeEnum,
-} from "@zbav-se.me/sdk/api/user";
 import { withTransactionQuery } from "@zbav-se.me/sdk/query/seller/transaction";
 import { withTransactionListingQuery } from "@zbav-se.me/sdk/query/seller/transaction-listing";
 import { withInboxQuery } from "~/client/@user/inbox/withInboxQuery";
+import { inboxArchiveFn } from "~/server/@user/inbox/fn/inboxArchiveFn";
 
 const terminalStatuses: tTransactionStatusEnum[] = [
 	tTransactionStatusEnum.rejected,
@@ -30,8 +23,8 @@ export namespace withArchiveBuyerMessageInboxMutation {
 
 export const withArchiveBuyerMessageInboxMutation = withMutation<
 	withArchiveBuyerMessageInboxMutation.Variables,
-	tApiInboxArchiveResponse[204],
-	apiInboxArchiveError
+	void,
+	Error
 >({
 	keys(variables) {
 		return [
@@ -45,21 +38,20 @@ export const withArchiveBuyerMessageInboxMutation = withMutation<
 		if (!terminalStatuses.includes(status)) {
 			return;
 		}
-		return withApi(
-			apiInboxArchive({
-				body: {
-					where: {
-						archivedAtIsNull: true,
-						family: tInboxFamilyEnum.transaction,
-						type: tInboxTypeEnum["buyer-message"],
-						referenceAllIn: [
-							transactionId,
-							listingId,
-						],
-					},
+
+		return inboxArchiveFn({
+			data: {
+				where: {
+					archivedAtIsNull: true,
+					family: "transaction",
+					type: "buyer-message",
+					referenceAllIn: [
+						transactionId,
+						listingId,
+					],
 				},
-			}),
-		);
+			},
+		});
 	},
 	invalidate: [
 		{
