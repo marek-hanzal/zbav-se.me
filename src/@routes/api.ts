254b5ba74318b1febda6ef365683af7a116962ce
Marek Hanzal <marek.hanzal@icloud.com>
2026-04-01 09:53:13 +0200

    Remove withLogMiddleware from API route to streamline middleware configuration

--- src/@routes/api.ts
+++ src/@routes/api.ts
@@ -1,11 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { withDatabaseMiddleware } from "~/server/middleware/withDatabaseMiddleware";
-import { withLogMiddleware } from "~/server/middleware/withLogMiddleware";
 
 export const Route = createFileRoute("/api")({
 	server: {
 		middleware: [
-			withLogMiddleware,
 			withDatabaseMiddleware,
 		],
 	},
