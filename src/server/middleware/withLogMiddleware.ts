e066bb1756c38ef30f46468279e9e561b4f2c780
Marek Hanzal <marek.hanzal@icloud.com>
2026-04-01 12:36:44 +0200

    Update logging configuration and dependencies
    - Added new logging dependencies: @logtape/file, @logtape/pretty, and @logtape/redaction for enhanced logging capabilities.
    - Updated .gitignore to include .logs and .nitro directories.
    - Refactored withLogMiddleware to utilize file logging with time rotation and improved console logging with redaction features.

--- src/server/middleware/withLogMiddleware.ts
+++ src/server/middleware/withLogMiddleware.ts
@@ -1,5 +1,14 @@
 import { AsyncLocalStorage } from "node:async_hooks";
-import { configure, getConsoleSink, getLogger, withContext } from "@logtape/logtape";
+import { getTimeRotatingFileSink } from "@logtape/file";
+import {
+	configure,
+	fingersCrossed,
+	getConsoleSink,
+	getLogger,
+	withContext,
+} from "@logtape/logtape";
+import { getPrettyFormatter } from "@logtape/pretty";
+import { redactByPattern } from "@logtape/redaction";
 import { createMiddleware } from "@tanstack/react-start";
 import { genId } from "@/lib/common/gen-id";
 
@@ -8,7 +17,45 @@ export const withLogMiddleware = createMiddleware().server(async ({ next }) => {
 		reset: true,
 		contextLocalStorage: new AsyncLocalStorage(),
 		sinks: {
-			console: getConsoleSink(),
+			file: getTimeRotatingFileSink({
+				nonBlocking: true,
+				directory: "./.logs",
+				interval: "hourly",
+				/**
+				 * 8hrs
+				 */
+				maxAgeMs: 8 * 60 * 60 * 1_000,
+			}),
+			console: fingersCrossed(
+				getConsoleSink({
+					formatter: redactByPattern(
+						getPrettyFormatter({
+							categoryWidth: 42,
+						}),
+						[
+							// {
+							// 	pattern: /postgresql:\/\//g,
+							// 	replacement(match, ...rest) {
+							// 		return `--${match}--`;
+							// 	},
+							// },
+						],
+					),
+					nonBlocking: true,
+				}),
+				{
+					triggerLevel: "trace",
+					//
+					isolateByCategory: "descendant",
+					isolateByContext: {
+						keys: [
+							"traceId",
+						],
+						bufferTtlMs: 300_000,
+						maxContexts: 128,
+					},
+				},
+			),
 		},
 		loggers: [
 			{
@@ -19,6 +66,7 @@ export const withLogMiddleware = createMiddleware().server(async ({ next }) => {
 				lowestLevel: "trace",
 				sinks: [
 					"console",
+					"file",
 				],
 			},
 			{
@@ -26,6 +74,7 @@ export const withLogMiddleware = createMiddleware().server(async ({ next }) => {
 				lowestLevel: "trace",
 				sinks: [
 					"console",
+					"file",
 				],
 			},
 			{
