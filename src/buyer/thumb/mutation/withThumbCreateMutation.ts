1a721af91d0f93201cfc2eefb38bd4b8e65f513e
Marek Hanzal <marek.hanzal@icloud.com>
2026-04-23 18:23:07 +0200

    Refine result validation in withThumbCreateMutation invalidation logic
    - Updated the invalidate function to check for a valid result before calling the update function on withListingQuery, ensuring that invalidation only occurs when appropriate.
    - This change enhances the robustness of the mutation handler by preventing unnecessary updates when no valid result is present.

--- src/buyer/thumb/mutation/withThumbCreateMutation.ts
+++ src/buyer/thumb/mutation/withThumbCreateMutation.ts
@@ -29,7 +29,7 @@ export const withThumbCreateMutation = withMutation<
 	invalidate: [
 		{
 			async invalidate(queryClient, result) {
-				withListingQuery.updateFn(queryClient, result);
+				result && withListingQuery.updateFn(queryClient, result);
 			},
 		},
 	],
