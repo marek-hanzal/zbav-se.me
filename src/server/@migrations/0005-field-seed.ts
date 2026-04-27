4a7e8048c07ddb7c04a43ce2941559b60f5b0854
Marek Hanzal <marek.hanzal@icloud.com>
2026-04-27 19:47:19 +0200

    Add field seed migration and initial field data
    - Introduced a new migration script to seed the `field` table with initial data from a JSON file.
    - The migration includes a function to generate unique IDs for each field entry, enhancing data integrity.
    - This update establishes a foundational dataset for the `field` table, supporting future functionalities.

---
+++ src/server/@migrations/0005-field-seed.ts
@@ -0,0 +1,23 @@
+import type { Migration } from "kysely";
+import { genId } from "@/lib/common/gen-id";
+import fieldSeedData from "~/server/@migrations/0005-field-seed/field.json" with {
+	type: "json",
+};
+
+export const FieldSeedMigration: Migration = {
+	async up(db) {
+		if (fieldSeedData && Object.keys(fieldSeedData).length > 0) {
+			await db
+				.insertInto("field")
+				.values(
+					Object.values(fieldSeedData).map((field) => ({
+						id: genId(),
+						name: field.name,
+						required: field.required,
+						group: field.group,
+					})),
+				)
+				.execute();
+		}
+	},
+};
