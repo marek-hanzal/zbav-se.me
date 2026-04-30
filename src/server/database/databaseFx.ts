166b186439008727c1f273917901379e28ff5b3b
Marek Hanzal <marek.hanzal@icloud.com>
2026-04-30 21:13:04 +0200

    Refactor database connection and translation synchronization logic
    - Updated the database connection pool size from 3 to 1 for optimized resource management.
    - Enhanced the translation synchronization process by integrating the `translationSyncFx` within the database migration workflow.
    - Improved the structure of the database effect to include necessary imports and streamline the translation loading mechanism.

--- src/server/database/databaseFx.ts
+++ src/server/database/databaseFx.ts
@@ -1,9 +1,16 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import { Effect } from "effect";
-import { MigrationContextFx, withDatabaseFx } from "@/lib/common/database";
+import { PostgresDialect } from "kysely";
+import Pool from "pg-pool";
+import { MigrationContextFx, withDatabaseFx, withDialectFx } from "@/lib/common/database";
 import { getRootLogger } from "~/common/log/getRootLogger";
+import { translationSyncFx } from "~/common/translation/server/fx/translationSyncFx";
 import { migrations } from "~/server/@migrations/migrations";
 import { runAuthMigration } from "~/server/auth/runAuthMigration";
 import type { Database } from "~/server/database/Database";
+import { ServerDatabaseSchema } from "../env/ServerDatabaseSchema";
+import { withKyselyFx } from "./fx/withKyselyFx";
 
 /**
  * Don't destructure stuff as there is Proxy
@@ -13,6 +20,27 @@ export const databaseFx = withDatabaseFx<Database>({
 		"db",
 	]),
 	async onPreMigration({ dialect }) {
+		{
+			const databaseConfig = ServerDatabaseSchema.parse(process.env);
+			const database = await databaseFx.pipe(
+				withDialectFx(
+					new PostgresDialect({
+						pool: new Pool({
+							connectionString: databaseConfig.SERVER_DATABASE_URL,
+							max: 1,
+						}),
+					}),
+				),
+				Effect.runPromise,
+			);
+
+			const dir = dirname(fileURLToPath(import.meta.url));
+
+			await translationSyncFx({
+				source: `${dir}/../@migrations/translation`,
+			}).pipe(withKyselyFx(database), Effect.runPromise);
+		}
+
 		await runAuthMigration(dialect);
 	},
 }).pipe(Effect.provideService(MigrationContextFx, migrations));
