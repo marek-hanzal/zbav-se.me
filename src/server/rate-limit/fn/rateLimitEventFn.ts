3b7a91521b6fa801ac8b53730a937dd47b174fa8
Marek Hanzal <marek.hanzal@icloud.com>
2026-05-22 08:11:24 +0200

    Created Fn for rate limit event

---
+++ src/server/rate-limit/fn/rateLimitEventFn.ts
@@ -0,0 +1,46 @@
+import { createServerFn } from "@tanstack/react-start";
+import { Effect } from "effect";
+import { zodGuardFx } from "@/lib/common/fx";
+import { withLoggerFx } from "@/lib/common/log";
+import { withDateFx } from "~/server/database/fx/withDateFx";
+import { withKyselyFx } from "~/server/database/fx/withKyselyFx";
+import { withDatabaseMiddleware } from "~/server/middleware/withDatabaseMiddleware";
+import { withLogMiddleware } from "~/server/middleware/withLogMiddleware";
+import { rateLimitEventFx } from "~/server/rate-limit/server/fx/rateLimitEventFx";
+import { RateLimitEventCreateSchema } from "~/server/rate-limit/server/schema/RateLimitEventCreateSchema";
+import { RateLimitEventSchema } from "~/server/rate-limit/server/schema/RateLimitEventSchema";
+
+export namespace rateLimitEventFn {
+	export type Error = Effect.Effect.Error<rateLimitEventFx>;
+}
+
+export const rateLimitEventFn = createServerFn()
+	.middleware([
+		withLogMiddleware,
+		withDatabaseMiddleware,
+	])
+	.inputValidator(RateLimitEventCreateSchema)
+	.handler(async ({ data, context: { database, rootLogger }, serverFnMeta: { name } }) => {
+		const logger = rootLogger.getChild([
+			"fn",
+			name,
+		]);
+		logger.trace(name, data);
+
+		return zodGuardFx({
+			schema: RateLimitEventSchema,
+			dataFx: rateLimitEventFx(data),
+		}).pipe(
+			withKyselyFx(database),
+			withDateFx,
+			withLoggerFx(rootLogger),
+			Effect.tapError((error) => {
+				return Effect.sync(() => {
+					logger.error(error._tag, {
+						error,
+					});
+				});
+			}),
+			Effect.runPromise,
+		);
+	});
