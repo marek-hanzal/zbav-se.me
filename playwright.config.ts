d79e8d1d225780e2ee9b0f9b078e4996b390785c
Marek Hanzal <marek.hanzal@icloud.com>
2026-05-26 15:21:04 +0200

    Updated e2e config

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	testDir: "./e2e",
 	globalSetup: "./e2e/init.ts",
 	outputDir: "./results",
-	// workers: 1,
+	workers: 1,
 	webServer: [
 		{
 			name: "App",
@@ -18,11 +18,12 @@ export default defineConfig({
 			env: {
 				...process.env,
 				SERVER_DATABASE_URL: "postgresql://postgres:postgres@localhost:55432/postgres",
+				SERVER_E2E: "e2e",
 			},
 		},
 	],
 	failOnFlakyTests: true,
-	fullyParallel: true,
+	fullyParallel: false,
 	reporter: process.env.CI
 		? [
 				[
