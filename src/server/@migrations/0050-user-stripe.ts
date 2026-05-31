72289aae153334936d5e1318686c6583eae28621
Marek Hanzal <marek.hanzal@icloud.com>
2026-05-31 21:09:19 +0200

    Added stripe migration

---
+++ src/server/@migrations/0050-user-stripe.ts
@@ -0,0 +1,30 @@
+import type { Migration } from "kysely/migration";
+
+export const UserStripeMigration: Migration = {
+	async up(db) {
+		await db.schema
+			.createTable("user_stripe")
+			.addColumn("id", "text", (col) => col.primaryKey().notNull())
+			.addColumn("userId", "text", (col) => col.notNull())
+			.addColumn("customerId", "text", (col) => col.notNull())
+			.addColumn("createdAt", "timestamptz", (col) => col.notNull())
+			.addForeignKeyConstraint(
+				"user_stripe_[userId]_fk",
+				[
+					"userId",
+				],
+				"user",
+				[
+					"id",
+				],
+				(c) => c.onDelete("cascade"),
+			)
+			.addUniqueConstraint("user_stripe_[userId]_unique_idx", [
+				"userId",
+			])
+			.addUniqueConstraint("user_stripe_[customerId]_unique_idx", [
+				"customerId",
+			])
+			.execute();
+	},
+};
--- src/server/@migrations/migrations.ts
+++ src/server/@migrations/migrations.ts
@@ -48,10 +48,11 @@ import { RateLimitRuleMigration } from "~/server/@migrations/0046-rate-limit-rul
 import { RateLimitEventMigration } from "~/server/@migrations/0047-rate-limit-event";
 import { ResourceDefinitionMigration } from "~/server/@migrations/0048-resource-definition";
 import { UserResourceLimitMigration } from "~/server/@migrations/0049-user-resource-limit";
-import { PlanMigration } from "~/server/@migrations/0050-plan";
-import { PlanResourceLimitMigration } from "~/server/@migrations/0051-plan-resource-limit";
-import { PlanResourceInventoryMigration } from "~/server/@migrations/0052-plan-resource-inventory";
-import { UserPlanMigration } from "~/server/@migrations/0053-user-plan";
+import { UserStripeMigration } from "~/server/@migrations/0050-user-stripe";
+import { PlanMigration } from "~/server/@migrations/0051-plan";
+import { PlanResourceLimitMigration } from "~/server/@migrations/0052-plan-resource-limit";
+import { PlanResourceInventoryMigration } from "~/server/@migrations/0053-plan-resource-inventory";
+import { UserPlanMigration } from "~/server/@migrations/0054-user-plan";
 
 export const migrations = {
 	"0000-postgres-extensions": PostgresExtensionsMigration,
@@ -104,8 +105,9 @@ export const migrations = {
 	"0047-rate-limit-event": RateLimitEventMigration,
 	"0048-resource-definition": ResourceDefinitionMigration,
 	"0049-user-resource-limit": UserResourceLimitMigration,
-	"0050-plan": PlanMigration,
-	"0051-plan-resource-limit": PlanResourceLimitMigration,
-	"0052-plan-resource-inventory": PlanResourceInventoryMigration,
-	"0053-user-plan": UserPlanMigration,
+	"0050-user-stripe": UserStripeMigration,
+	"0051-plan": PlanMigration,
+	"0052-plan-resource-limit": PlanResourceLimitMigration,
+	"0053-plan-resource-inventory": PlanResourceInventoryMigration,
+	"0054-user-plan": UserPlanMigration,
 };
