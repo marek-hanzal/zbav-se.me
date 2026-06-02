f495c9980e71769b5c52cf6982ac9f9840a79131
Marek Hanzal <marek.hanzal@icloud.com>
2026-06-02 09:58:19 +0200

    Added user event cleanup cron

--- src/user/user-event/server/cron/withUserEventCleanupCronFx.ts
+++ src/user/user-event/server/cron/withUserEventCleanupCronFx.ts
@@ -1,7 +1,39 @@
 import { Effect } from "effect";
+import { DateContextFx } from "@/lib/common/date";
+import { getLoggerFx } from "@/lib/common/log";
+import { dbFx } from "~/server/database/fx/dbFx";
+import { withTransactionFx } from "~/server/database/fx/withTransactionFx";
 
 export const withUserEventCleanupCronFx = Effect.fn("userEventCleanupCronFx")(function* () {
-	//
+	const logger = yield* getLoggerFx("userEventCleanupCronFx", "cron");
+	logger.trace("userEventCleanupCronFx");
+
+	yield* withTransactionFx(
+		Effect.gen(function* () {
+			const dateContext = yield* DateContextFx;
+
+			yield* dbFx(async (kysely) => {
+				const source = kysely
+					.selectFrom("user_event as ue")
+					.select("ue.id")
+					.where(
+						"ue.createdAt",
+						"<=",
+						dateContext
+							.now()
+							.minus({
+								months: 6,
+							})
+							.toJSDate(),
+					)
+					.orderBy("ue.createdAt", "asc")
+					.orderBy("ue.id", "asc")
+					.limit(50_000);
+
+				return kysely.deleteFrom("user_event").where("id", "in", source).execute();
+			});
+		}),
+	);
 });
 
 export type withUserEventCleanupCronFx = ReturnType<typeof withUserEventCleanupCronFx>;
