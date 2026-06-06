9c3e769d131cdc25b32ddaee989699b99621319b
Marek Hanzal <marek.hanzal@icloud.com>
2026-06-06 12:11:33 +0200

    Updated available resource definition

--- src/common/resource-definition/enum/ResourceDefinitionEnumSchema.ts
+++ src/common/resource-definition/enum/ResourceDefinitionEnumSchema.ts
@@ -1,25 +1,56 @@
 import { z } from "zod";
 
+const SellerEnumSchema = z.enum([
+	/**
+	 * How many listing a user can created (include drafts)
+	 */
+	"seller:limit:listing.count",
+	/**
+	 * How many gallery items may a user attach to the listing (draft)
+	 */
+	"seller:limit:listing.gallery.count",
+	/**
+	 * By default, listings are delivered to non-paying buyers with delay, this perk
+     * enables seller to distribute their listings to _all_ users in time.
+	 */
+	"seller:limit:listing.early-delivery",
+]);
+
+const BuyerEnumSchema = z.enum([
+	/**
+	 * How many feeds a user can have
+	 */
+	"buyer:limit:feed.count",
+    /**
+     * Buyer-side counter-part for sellers' early-delivery.
+     *
+     * This per enables buyers to discover all the listings in the feed early; does not effect
+     * direct links which are enabled all the times.
+     */
+	"buyer:limit:listing.early-discovery",
+	/**
+	 * Early Diescovery
+	 *
+	 * This enables users to _discover_ listings early before others; basically it removes
+	 * synthetic availableAt in the future on listings.
+	 */
+	"buyer:limit:early-discovery",
+]);
+
+const CommonEnumSchema = z.enum([
+	/**
+	 * Those are token packages
+	 */
+	"common:item:token-small",
+	"common:item:token-medium",
+	"common:item:token-large",
+]);
+
 export const ResourceDefinitionEnumSchema = z
-	.enum([
-		/**
-		 * How many listing a user can created (include drafts)
-		 */
-		"listing.count",
-		/**
-		 * How many gallery items may a user attach to the listing (draft)
-		 */
-		"listing.gallery.count",
-		/**
-		 * How many feeds a user can have
-		 */
-		"feed.count",
-		/**
-		 * Those are token packages
-		 */
-		"item:token-small",
-		"item:token-medium",
-		"item:token-large",
+	.union([
+		SellerEnumSchema,
+		BuyerEnumSchema,
+		CommonEnumSchema,
 	])
 	.meta({
 		id: "ResourceDefinitionEnum",
