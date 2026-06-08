17024cea78cba3de6bff82af7cf6e36f304dea5e
Marek Hanzal <marek.hanzal@icloud.com>
2026-06-08 20:22:54 +0200

    Blag

--- src/user/resource-bundle/server/fx/userResourceBundleCreateFx.ts
+++ src/user/resource-bundle/server/fx/userResourceBundleCreateFx.ts
@@ -3,8 +3,10 @@ import { DateServiceFx } from "@/lib/common/date";
 import { genId } from "@/lib/common/gen-id";
 import { getLoggerFx } from "@/lib/common/log";
 import { dbFx } from "~/server/database/fx/dbFx";
+import { withTransactionFx } from "~/server/database/fx/withTransactionFx";
 import { InvalidRequestErrorFx } from "~/server/error/InvalidRequestErrorFx";
 import type { UserResourceBundleCreateSchema } from "../schema/UserResourceBundleCreateSchema";
+import { userResourceBundleMaterializeFx } from "./userResourceBundleMaterializeFx";
 
 export namespace userResourceBundleCreateFx {
 	export interface Props extends UserResourceBundleCreateSchema.Type {
@@ -30,52 +32,75 @@ export const userResourceBundleCreateFx = Effect.fn("userResourceBundleCreateFx"
 	const now = dateService.now().toJSDate();
 	const activeFrom = availableAt ?? now;
 
-	const resourceBundle = yield* dbFx(async (kysely) => {
-		return kysely
-			.selectFrom("resource_bundle")
-			.select([
-				"id",
-			])
-			.where("name", "=", bundle)
-			.executeTakeFirst();
-	});
-
-	if (!resourceBundle) {
-		return yield* new InvalidRequestErrorFx({
-			message: `Resource bundle [${bundle}] does not exist`,
-		});
-	}
-
-	yield* dbFx(async (kysely) => {
-		return kysely
-			.insertInto("user_resource_bundle")
-			.values({
-				id: genId(),
-				userId,
-				resourceBundleId: resourceBundle.id,
-				createdAt: now,
-				availableAt: activeFrom,
-				expiresAt,
-			})
-			.onConflict((oc) =>
-				oc
-					.columns([
-						"userId",
-						"resourceBundleId",
+	return yield* withTransactionFx(
+		Effect.gen(function* () {
+			const resourceBundle = yield* dbFx(async (kysely) => {
+				return kysely
+					.selectFrom("resource_bundle")
+					.select([
+						"id",
 					])
-					.doNothing(),
-			)
-			.execute();
-	});
+					.where("name", "=", bundle)
+					.executeTakeFirst();
+			});
 
-	return yield* dbFx(async (kysely) => {
-		return kysely
-			.selectFrom("user_resource_bundle")
-			.selectAll()
-			.where("userId", "=", userId)
-			.where("resourceBundleId", "=", resourceBundle.id)
-			.executeTakeFirstOrThrow();
-	});
+			if (!resourceBundle) {
+				return yield* new InvalidRequestErrorFx({
+					message: `Resource bundle [${bundle}] does not exist`,
+				});
+			}
+
+			const inserted = yield* dbFx(async (kysely) => {
+				return kysely
+					.insertInto("user_resource_bundle")
+					.values({
+						id: genId(),
+						userId,
+						resourceBundleId: resourceBundle.id,
+						createdAt: now,
+						availableAt: activeFrom,
+						expiresAt,
+					})
+					.onConflict((oc) =>
+						oc
+							.columns([
+								"userId",
+								"resourceBundleId",
+							])
+							.doNothing(),
+					)
+					.returning([
+						"id",
+					])
+					.executeTakeFirst();
+			});
+
+			if (inserted) {
+				yield* userResourceBundleMaterializeFx({
+					resourceBundleId: resourceBundle.id,
+					userResourceBundleId: inserted.id,
+					createdAt: now,
+					availableAt: activeFrom,
+					...(expiresAt === null
+						? {}
+						: {
+								featureExpiresAt: expiresAt,
+								itemExpiresAt: expiresAt,
+								limitExpiresAt: expiresAt,
+							}),
+				});
+			}
+
+			return yield* dbFx(async (kysely) => {
+				return kysely
+					.selectFrom("user_resource_bundle")
+					.selectAll()
+					.where("userId", "=", userId)
+					.where("resourceBundleId", "=", resourceBundle.id)
+					.executeTakeFirstOrThrow();
+			});
+		}),
+	);
 });
 
 export type userResourceBundleCreateFx = ReturnType<typeof userResourceBundleCreateFx>;
